bbcb6550670ec2500d68493cb2dba5c0942b447f
Valeria Stamenova <stam0012@hz.nl>
2023-07-03 19:04:54 +0200

    Create slideshow component and attach it to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,9 +27,11 @@
                             <p class="uppercase pt-2 pl-20 text-gray-200">sponsored by [logo]</p>
                         </div>--}}
                     </div>
-                    <div class="mt-16 flex flex-col items-center">
-                        <x-custom-button-link href="{{ route('register') }}">Register now</x-custom-button-link>
-                    </div>
+                    @guest()
+                        <div class="mt-16 flex flex-col items-center">
+                            <x-custom-button-link href="{{ route('register') }}">Register now</x-custom-button-link>
+                        </div>
+                    @endguest
                 </div>
             </div>
             <!-- Blob -->
@@ -38,55 +40,27 @@
                  style="transform: rotate(110deg) scaleX(-1)">
         </div>
         <!-- Second banner -->
-        <div class="relative isolate py-72 bg-cover bg-center"
-             style="background-image: url('/img/auditorium.jpg')">
-            <!-- Gradient -->
-            <div class="absolute inset-0 bg-blue-500 opacity-90"></div>
+        <div class="relative isolate h-[29rem] border-red-700">
             <!-- Blob -->
             <img src="/img/blue-blob.png"
                  class="absolute -top-96 -left-48 h-[34rem] transform translate-x-1/2 translate-y-1/2 z-10  opacity-75"
                  style="transform: rotate(61deg)">
-
-            <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                <div>
-                    <h2 class="text-white text-3xl sm:text-4xl font-bold text-center whitespace-nowrap"
-                        style="text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);">
-                        What is this conference?
-                    </h2>
-                    <p class="text-white text-center" style="text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);">
-                        We are so proud to present you with this inspirational and fun conference for and by you
-                        all!
-                        Teachers, students and our valued company representatives have come together to create this
-                        special program. Talk to companies about their projects in the presentation market.
-                        Experiment,
-                        learn and play around during the workshops and get motivated by the various talks. There is
-                        enough for everyone!
-                    </p>
-                </div>
-            </div>
+            <x-slideshow/>
         </div>
         <!-- Third banner + gradient (since empty bg) -->
         <div
             class="relative isolate py-72 bg-cover bg-center before:absolute before:inset-0 before:bg-gradient-to-br before:from-gradient-purple before:via-red-500 before:to-orange-500 before:opacity-70">
             <!-- Blob -->
             <img src="/img/red-blob.png"
-                 class="absolute -top-80 right-0 h-[34rem] transform translate-x-1/2 translate-y-1/2 z-10  opacity-75"
+                 class="absolute -top-48 right-0 h-[25rem] transform translate-x-1/2 translate-y-1/2 z-10  opacity-75"
                  style="transform: rotate(61deg)">
 
             <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <div>
                     <h2 class="text-white text-4xl sm:text-5xl font-bold text-center whitespace-nowrap hover:animate-pulse"
                         style="text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);">
-                        Get to know more
+                        Sponsors
                     </h2>
-                </div>
-                <div class="flex justify-center mt-4">
-                    <p class="text-white text-xl mr-7 hover:underline hover:text-yellow-500 transition-all">Check
-                                                                                                            out
-                                                                                                            our
-                                                                                                            speakers</p>
-                    <p class="text-white text-xl hover:underline hover:text-yellow-500 transition-all">Check out the
-                                                                                                       companies</p>
                 </div>
             </div>
         </div>
