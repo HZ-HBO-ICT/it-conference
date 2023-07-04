c6c72ddd957e9ad646dcf1ea0959f02ebbc41db2
v-stamenova <valeria.stamenova3@gmail.com>
2023-07-04 23:28:03 +0200

    Add `RoleSeeder` to be called by `DatabaseSeeder`

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            SpeakerSeeder::class
+            SpeakerSeeder::class,
+            RoleSeeder::class
         ]);
     }
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
 {
