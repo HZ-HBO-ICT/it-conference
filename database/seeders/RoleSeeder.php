e11807981d6deed5a2f33d6588904c1aa224824f
v-stamenova <valeria.stamenova3@gmail.com>
2023-07-04 23:25:05 +0200

    Add participant and company representative as roles

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create(['name' => 'participant']);
+        Role::create(['name' => 'company representative']);
+    }
+}
