433d4c43aae35a8eb13e6ae86756a404435b8a88
TimKardol <tnmkardol@outlook.com>
2023-08-11 23:13:53 +0200

    Update the FAQ page

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,28 +1,43 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Frequently Asked Questions') }}
-        </h2>
-        <p class="mt-2">
-            Have a different question and can't find the answer you're looking for? 
-            <br>
-            Reach out to our team at <a class="text-blue-600 visited:text-purple-600" href="mailto:user@email.nl">user@email.nl</a> and we'll get back to you as soon as we can.
-        </p>
-    </x-slot>
-
-    <div class="flex justify-center">
-        <dl class="grid grid-cols-2 gap-x-10 gap-y-16">
-            <div>
-                <dt class="leading-7 ">Test</dt>
-                <dd class="">test</dd>
-            </div>
-            <div>
-                <dt class=""></dt>
-                <dd class="">test</dd>
-            </div>
+    <div class="px-6 py-6 max-w-7xl mx-auto mt-5 mb-5 border rounded bg-white">
+        <div class="text-center max-w-2xl mx-auto">
+            <h2 class="tracking-tight leading-10 font-bold text-2xl">Frequently Asked Questions</h2>
+            <p class="leading-7 text-base">
+                Have a different question and can’t find the answer you’re looking for? Reach out to our support team by emailing 
+                <a class="text-blue-600 hover:text-blue-400 visited:text-purple-600" href="mailto: info@weareinittogether.nl">info@weareinittogether.nl</a>
+                and we’ll get back to you as soon as we can.
+            </p>
+        </div>
+        <div class="m-20">
+            <dl class="gap-x-10 gap-y-16 grid-cols-2 grid ">
+                <div>
+                    <dt class="leading-7 font-semibold text-base">What is the “We are in IT together Conference”?</dt>
+                    <dd class="leading-7 mt-2">The 'We are in IT together Conference' is an inclusive platform where students, teachers, and IT company representatives collaborate through workshops, presentations, and booths. This event promotes knowledge exchange, enabling students to explore the dynamic IT industry and providing companies opportunities to connect with potential employees, offer internships, and engage with the university's educational approach.</dd>
+                </div>
+                <div>
+                    <dt class="leading-7 font-semibold text-base">When and where does the “We are in IT together Conference” take place?</dt>
+                    <dd class="leading-7 mt-2">The conference will take place on November 18th at our location in Het Groenewoud Middelburg. Visit 
+                    <a href="https://hz.nl/en/about-hz/contact/middelburg"> hz.nl </a> for information about parking.</dd>
+                </div>
+                <div>
+                    <dt class="leading-7 font-semibold text-base">Can I follow the conference online?</dt>
+                    <dd class="leading-7 mt-2">Unfortunately, we will not be able to provide online attendance or record sessions.</dd>
+                </div>
+                {{-- This should be left out or link directly to our contact page. The email address is mentioned at the top of this page.
+                    <div>
+                    <dt class="leading-7 font-semibold text-base">How can I contact the “We are in IT together Conference” team?</dt>
+                    <dd class="leading-7 mt-2">You can contact our team by sending an email to info@weareinittogether.nl.</dd>
+                </div> 
+                --}}
+                <div>
+                    <dt class="leading-7 font-semibold text-base">Can I join the conference as a participant?</dt>
+                    <dd class="leading-7 mt-2">You can if you are a HZ-ICT student, Scalda student, an Alumni HZ-ICT student, a company with a dedicated IT department or a member of our partnering company representatives. You can register using this website as soon as registrations have opened.</dd>
+                </div>
+                <div>
+                    <dt class="leading-7 font-semibold text-base">Can I be a partner with the 'We are in IT together Conference'?</dt>
+                    <dd class="leading-7 mt-2">If you work in the field of ICT, we would very much appreciate you wanting to join us and have our students get acquainted with your IT company. You can apply through the form to join the program.</dd>
+                </div>
+            </dl>
         </div>
     </div>
-
-
-    
 </x-app-layout>
