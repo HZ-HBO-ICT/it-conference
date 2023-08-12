e5e5134521ca9d0100e210b2bc9ee5c9d68f800d
Valeria Stamenova <stam0012@hz.nl>
2023-08-12 21:14:56 +0200

    Add a check if the company is approved before being able to manage employees

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -4,7 +4,12 @@
             {{$team->name}} Team details
         </h2>
         <h2 class="text-md text-gray-800 dark:text-gray-200 leading-tight pt-3">
-            Here you can manage the employees that will be present during the conference
+            @if($team->is_approved)
+                Here you can manage the company details and the employees that will be present during the conference
+            @else
+                Here you can manage the company details.<br><span class="underline">Managing the employees that will be joining you during the conference is only avaiable after
+                your company is approved.</span>
+            @endif
         </h2>
     </x-slot>
 
@@ -12,10 +17,12 @@
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @livewire('teams.update-team-name-form', ['team' => $team])
 
+            @if($team->is_approved)
             @livewire('teams.team-member-manager', ['team' => $team])
+            @endif
 
             @if (Gate::check('delete', $team) && ! $team->personal_team)
-                <x-section-border />
+                <x-section-border/>
 
                 <div class="mt-10 sm:mt-0">
                     @livewire('teams.delete-team-form', ['team' => $team])
