dfe627e39602cf3a2c9d285b11149519a8283739
Valeria Stamenova <stam0012@hz.nl>
2023-08-15 13:11:35 +0200

    Create views and methods to handle the invitation

--- resources/views/teams/create-team-form.blade.php
+++ resources/views/teams/create-team-form.blade.php
@@ -1,30 +1,46 @@
 <x-form-section submit="createTeam">
     <x-slot name="title">
-        {{ __('Team Details') }}
+        {{ __('Company Details') }}
     </x-slot>
 
     <x-slot name="description">
-        {{ __('Create a new team to collaborate with others on projects.') }}
+        {{ __('Add manually a new company that will join the conference. An email with invitation to join as company rep will be sent on the company representative email you specify bellow') }}
     </x-slot>
 
     <x-slot name="form">
-        <div class="col-span-6">
-            <x-label value="{{ __('Team Owner') }}" />
-
-            <div class="flex items-center mt-2">
-                <img class="w-12 h-12 rounded-full object-cover" src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}">
-
-                <div class="ml-4 leading-tight">
-                    <div class="text-gray-900 dark:text-white">{{ $this->user->name }}</div>
-                    <div class="text-gray-700 dark:text-gray-300 text-sm">{{ $this->user->email }}</div>
-                </div>
-            </div>
-        </div>
-
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="name" value="{{ __('Team Name') }}" />
-            <x-input id="name" type="text" class="mt-1 block w-full" wire:model.defer="state.name" autofocus />
-            <x-input-error for="name" class="mt-2" />
+            <x-label for="name" value="{{ __('Company Name') }}"/>
+            <x-input id="name" type="text" class="mt-1 block w-full" wire:model.defer="state.name" autofocus/>
+            <x-input-error for="name" class="mt-2"/>
+        </div>
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="description" value="{{ __('Company Description') }}"/>
+            <textarea id="description" wire:model.defer="state.description"
+                      class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full"
+                      name="description" required
+            >{{old('company_description')}}</textarea>
+        </div>
+        <!-- TODO: Once the company address is fixed with proper fields, refactor here and in the CreateTeam class -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="address" value="{{ __('Company Address') }}"/>
+            <x-input id="address" type="text" class="mt-1 block w-full" wire:model.defer="state.address" autofocus/>
+            <x-input-error for="address" class="mt-2"/>
+        </div>
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="website" value="{{ __('Company Website') }}"/>
+            <x-input id="website" type="text" class="mt-1 block w-full" wire:model.defer="state.website" autofocus/>
+            <x-input-error for="website" class="mt-2"/>
+        </div>
+        <div class="col-span-6 sm:col-span-4">
+            <x-section-border />
+            <x-label for="rep_name" value="{{ __('Company Representative Name') }}"/>
+            <x-input id="rep_name" type="text" class="mt-1 block w-full" wire:model.defer="state.rep_name" autofocus/>
+            <x-input-error for="rep_name" class="mt-2"/>
+        </div>
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="rep_email" value="{{ __('Company Representative Email') }}"/>
+            <x-input id="rep_email" type="email" class="mt-1 block w-full" wire:model.defer="state.rep_email" autofocus/>
+            <x-input-error for="rep_email" class="mt-2"/>
         </div>
     </x-slot>
 
--- resources/views/teams/create.blade.php
+++ resources/views/teams/create.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
+    <!-- TODO: Once the new content moderator layout is approved, refactor the design -->
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Create Team') }}
+            {{ __('Add a company') }}
         </h2>
     </x-slot>
 
