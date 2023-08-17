afe8e9ec6123437c85ebf95d0fed0ffab50601c9
IGORnvk <71547534+IGORnvk@users.noreply.github.com>
2023-08-17 16:08:40 +0200

    changed name of a button

--- resources/views/myhub/programme-register.blade.php
+++ resources/views/myhub/programme-register.blade.php
@@ -47,7 +47,7 @@
 
                 <a href="{{ route('my-programme') }}" type="button"
                    class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-16">
-                    Save
+                    Go back
                 </a>
             </div>
         </div>
