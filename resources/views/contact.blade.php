68d3754ab26a9289a2cae3837bdeb9e66d561c35
TimKardol <tnmkardol@outlook.com>
2023-08-20 14:29:06 +0200

    Add dark mode classes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,9 +3,9 @@
         <div class="py-40 px-8 max-w-7xl mx-auto">
             <div class="gap-8 grid-cols-12 grid">
                 <div class="col-span-5">
-                    <h2 class="tracking-tight leading-10 font-bold text-2xl">Contact Information</h2>
-                    <p>Het Groene Woud 1-3</p>
-                    <p>4331 NB Middelburg</p>
+                    <h2 class="tracking-tight leading-10 font-bold text-2xl dark:text-white">Contact Information</h2>
+                    <p class="dark:text-gray-200">Het Groene Woud 1-3</p>
+                    <p class="dark:text-gray-200">4331 NB Middelburg</p>
                     <a class="text-blue-600 hover:text-blue-400 visited:text-purple-600" href="mailto: info@weareinittogether.nl">info@weareinittogether.nl</a>
                 </div>
             </div>
