8f527a91669d44599cd5608c5413096b741f85f9
Tim Kardol <tnmkardol@outlook.com>
2023-08-24 22:20:21 +0200

    Add structure for 'profile' cards

--- resources/views/companies.blade.php
+++ resources/views/companies.blade.php
@@ -1,16 +1,16 @@
 <x-app-layout>
     @if(!$teams->isEmpty())
     <div class="px-6 py-6 max-w-7xl mx-auto mt-5 border border-gray-100 rounded bg-white dark:bg-gray-800 dark:border-gray-700">
-        <div class="text-center max-w-2xl mx-auto">
-            <h2 class="tracking-tight leading-10 font-bold text-2xl dark:text-white">Companies of 2023</h2>
+        <div class="text-center max-w-2xl mx-auto mb-5">
+            <h2 class="tracking-tight leading-10 font-bold text-2xl dark:text-white">Conference Line-up</h2>
         </div>
-            <ul class="gap-y-16 gap-x-8 grid-cols-2 max-w-2xl grid mt-20 mx-auto" role="list">
+            <ul class="gap-x-8 gap-y-8 grid-cols-3 max-w-none mx-0 grid" role="list">
                 @foreach ($teams as $team)
-                <li class="border rounded border-gray-300">
-                    <img class="object-cover rounded-2xl w-full aspect-auto[3/2] h-auto" src="TODO: when variable name is known" alt="Logo of {{$team->name}}">
-                    <h3 class="tracking-tight leading-8 font-semibold text-lg mt-6">{{$team->name}}</h3>
-                    <a class="" href="//{{$team->website}}">{{$team->website}}</a>
-                </li>
+                    <li class="px-10 py-8 rounded-2xl border border-gray-300 shadow">
+                        <img class="w-56 h-56 rounded-full mx-auto my-auto max-w-full block" src="./img/teacher.png" alt="Logo of {{$team->name}}">
+                        <h3 class="tracking-tight leading-7 font-semibold text-base mt-6 text-center">{{$team->name}}</h3>
+                        <p class="leading-6 text-sm text-center">{{$team->description}}</p>
+                    </li>
                 @endforeach
             </ul>
         </div>
