635565415278a78a0b21ba37c567a568ae2643d7
IGORnvk <71547534+IGORnvk@users.noreply.github.com>
2023-09-02 20:25:33 +0200

    test fix

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\SponsorTier;
+use App\Models\Team;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,6 +14,8 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        SponsorTier::factory()->has(Team::factory())->create();
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
