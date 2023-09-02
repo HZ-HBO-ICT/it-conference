47e465ecd1bfc6c6679b5602edaa57d3690d7d14
TimKardol <tnmkardol@outlook.com>
2023-09-02 21:58:08 +0200

    Add new cards to overview

--- resources/views/moderator/overview.blade.php
+++ resources/views/moderator/overview.blade.php
@@ -7,44 +7,39 @@
 
 <x-content-moderator-layout>
     <div>
-        <h2 class="text-4xl font-extrabold text-gray-700 dark:text-white ml-4 py-5">Overview</h2>
-        <h2 class="text-3xl font-extrabold text-gray-700 dark:text-white ml-4">Requests</h2>
-        <div class="grid grid-cols-3 pr-10 pt-5">
-            <x-content-moderator-block :label="'Company requests'" :count="Team::where('is_approved', 0)->count()"
-                                       :routeName="route('moderator.requests', 'booths')"/>
-            <x-content-moderator-block :label="'Booth requests'" :count="Booth::where('is_approved', 0)->count()"
-                                       :routeName="route('moderator.requests', 'booths')"/>
-            <x-content-moderator-block :label="'Sponsorship requests'"
-                                       :count="Team::where('is_sponsor_approved', 0)->count()"
-                                       :routeName="route('moderator.requests', 'sponsorships')"/>
-        </div>
-        <div class="grid grid-cols-2 pt-5">
-            <x-content-moderator-block :label="'Presentation requests'" :count="$numberOfPresentationRequests"
-                                       :routeName="route('moderator.requests', 'presentations')"/>
-            <div class="pr-10">
-                <x-content-moderator-block :label="'Presentations to be scheduled'"
-                                           :count="$numberOfUnscheduledPresentations"
-                                           :routeName="route('moderator.presentations-for-scheduling')"/>
+        <div class="py-8 px-2 mx-auto max-w-7xl">
+            <div>
+                <h3 class="leading-6 font-semibold text-xl">Pending requests</h3>
+                <div class="pt-6 px-6 pb-12 rounded-lg overflow-hidden relative">
+                    <dl class="gap-5 grid-cols-3 grid mt-5">
+                        <x-content-moderator-block 
+                            :label="'Companies'" 
+                            :count="Team::where('is_approved', 0)->count()"
+                            :routeName="route('moderator.requests', 'booths')"
+                            :icon="'M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z'"/>
+                        <x-content-moderator-block 
+                            :label="'Booths'" 
+                            :count="Booth::where('is_approved', 0)->count()"
+                            :routeName="route('moderator.requests', 'booths')"
+                            :icon="'M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z'"/>
+                        <x-content-moderator-block 
+                            :label="'Sponsorships'"
+                            :count="Team::where('is_sponsor_approved', 0)->count()"
+                            :routeName="route('moderator.requests', 'sponsorships')"
+                            icon="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"/>
+                        <x-content-moderator-block 
+                            :label="'Presentations'"
+                            :count="$numberOfPresentationRequests"
+                            :routeName="route('moderator.requests', 'presentations')"
+                            icon="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"/>
+                        <x-content-moderator-block 
+                            :label="'Schedule presentations'"
+                            :count="$numberOfUnscheduledPresentations"
+                            :routeName="route('moderator.presentations-for-scheduling')"
+                            :icon="'M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5'"/>
+                    </dl>
+                </div>
             </div>
-        </div>
-        <div class="flex items-center justify-center pr-10">
-            <div class="border-t border-gray-200 dark:border-gray-600 mt-5 w-4/5"></div>
-        </div>
-        <h2 class="text-3xl font-extrabold text-gray-700 dark:text-white mt-6 ml-4">Stats</h2>
-        <div class="grid grid-cols-3 pr-10 pt-5 gap-y-4">
-            <x-content-moderator-overview-block :label="'Participants'" :count="User::role('participant')->get()->count()"
-                                                :route="route('moderator.list', 'participants')"/>
-            <x-content-moderator-overview-block :label="'Speakers'" :count="User::role('speaker')->get()->count()"
-                                                :route="route('moderator.list', 'speakers')"/>
-            <x-content-moderator-overview-block :label="'All users'" :count="User::all()->count()"
-                                                :route="route('moderator.list', 'users')"/>
-            <x-content-moderator-overview-block :label="'Booths'" :count="Booth::where('is_approved', 1)->count()"
-                                                :route="route('moderator.list', 'booths')"/>
-            <x-content-moderator-overview-block :label="'Presentations'"
-                                                :count="$numberOfScheduledPresentations"
-                                                :route="route('moderator.list', 'presentations')"/>
-            <x-content-moderator-overview-block :label="'Companies'" :count="Team::where('is_approved', 1)->count()"
-                                                :route="route('moderator.list', 'teams')"/>
         </div>
     </div>
 </x-content-moderator-layout>
