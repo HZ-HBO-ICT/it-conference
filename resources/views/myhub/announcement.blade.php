8de57ceb65494b509aadfabe617fc676604db034
Valeria Stamenova <stam0012@hz.nl>
2023-09-09 19:28:45 +0200

    Alter announcements to behave like home - notifications will be added later

--- resources/views/myhub/announcement.blade.php
+++ resources/views/myhub/announcement.blade.php
@@ -1,11 +1,28 @@
-<x-hub-layout>
-    {{--announcements--}}
-    <div class="z-20 w-full mr-8">
-        <div class="bg-white dark:bg-gray-800 h-fit overflow-hidden rounded-lg shadow-xl">
-            <div class="px-6 py-6 bg-gray-50 dark:bg-gray-800">
-                <h2 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight">Announcements</h2>
+@php
+    use App\Models\Team;
+    use App\Models\Booth;
+    use App\Models\Speaker;
+    use App\Models\User;
+@endphp
 
-                <p class="text-l pt-16 text-gray-800 dark:text-gray-200">There are no new notifications!</p>
+<x-hub-layout>
+    <div>
+        <div class="py-8 px-2 mx-auto max-w-7xl">
+            <div>
+                <h3 class="leading-6 font-semibold text-xl dark:text-white">Dashboard</h3>
+                <div class="pt-6 px-6 pb-12 rounded-lg overflow-hidden relative">
+                    <dl class="gap-5 grid-cols-3 grid mt-5">
+                       <x-company-rep-block
+                            :label="'Company status'"
+                            :icon="'M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z'"/>
+                        <x-company-rep-block
+                            :label="'Booth status'"
+                            :icon="'M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z'"/>
+                    <x-company-rep-block
+                            :label="'Sponsorship status'"
+                            :icon="'M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z'"/>
+                    </dl>
+                </div>
             </div>
         </div>
     </div>
