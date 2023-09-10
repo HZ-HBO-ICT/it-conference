183cb3c0ebe3c258acfdbcb4eaa4cab433bb5c51
Valeria Stamenova <stam0012@hz.nl>
2023-09-10 16:51:05 +0200

    Make request presentation accessible through the sidebar

--- resources/views/layouts/myhub.blade.php
+++ resources/views/layouts/myhub.blade.php
@@ -1,3 +1,14 @@
+@php
+    use Illuminate\Support\Facades\Auth;
+    use App\Models\Presentation;
+
+    $canRequestPresentation = true;
+    if (Auth::user()->cannot('sendRequest', Presentation::class)
+        && Auth::user()->cannot('sendRequestGoldenSponsor', Presentation::class)) {
+        $canRequestPresentation = false;
+    }
+@endphp
+
 <x-app-moderator-layout>
     <div class="flex">
         <div class="flex-col w-72 flex z-50 inset-y-0 relative bg-white dark:bg-gray-900 h-screen">
@@ -9,7 +20,7 @@
                 </div>
                 <nav class="flex-col flex-1 flex">
                     <ul class="gap-y-3 flex-col flex-1 flex" role="list">
-                        <!-- Content Management Links -->
+                        <!-- Company Links -->
                         <li>
                             <ul class="-mx-2" role="list">
                                 <x-sidebar-link
@@ -18,21 +29,47 @@
                                     :icon="'M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'"/>
                             </ul>
                         </li>
-                        <li>
-                            <div class="leading-6 font-semibold text-xs text-gray-400">Company</div>
-                            <ul class="-mx-2" role="list">
-                                <x-sidebar-link
-                                    :label="'Company details'"
-                                    :route="'teams.show'"
-                                    :icon="'M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z'"
-                                    :param="Auth::user()->currentTeam->id"/>
-                                <x-sidebar-link
-                                    :label="'Requests'"
-                                    :route="'teams.requests'"
-                                    :icon="'M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z'"
-                                    :param="Auth::user()->currentTeam"/>
-                            </ul>
-                        </li>
+                        @if(Auth::user()->currentTeam)
+                            <li>
+                                <div class="leading-6 font-semibold text-xs text-gray-400">Company</div>
+                                <ul class="-mx-2" role="list">
+                                    <x-sidebar-link
+                                        :label="'Company details'"
+                                        :route="'teams.show'"
+                                        :icon="'M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z'"
+                                        :param="Auth::user()->currentTeam->id"/>
+                                    @if(Auth::user()->currentTeam->owner->id == Auth::user()->id)
+                                        <x-sidebar-link
+                                            :label="'Requests'"
+                                            :route="'teams.requests'"
+                                            :icon="'M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z'"
+                                            :param="Auth::user()->currentTeam"/>
+                                    @endif
+                                    @if($canRequestPresentation)
+                                        @unlessrole('content moderator')
+                                        <x-sidebar-link
+                                            :label="'Request presentation'"
+                                            :route="'speakers.request.presentation'"
+                                            :icon="'M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6'"/>
+                                        @endunlessrole
+                                    @endif
+                                </ul>
+                            </li>
+                        @else
+                            @if($canRequestPresentation)
+                            @unlessrole('content moderator')
+                                <li>
+                                    <ul class="-mx-2" role="list">
+                                        <x-sidebar-link
+                                            :label="'Request presentation'"
+                                            :route="'speakers.request.presentation'"
+                                            :icon="'M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6'"/>
+                                    </ul>
+                                </li>
+                            @endunlessrole
+                            @endif
+                        @endif
+
                         <!-- Profile Links -->
                         <li>
                             <div class="leading-6 font-semibold text-xs text-gray-400">Profile</div>
