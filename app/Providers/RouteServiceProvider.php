d3d58f43dc87e38fa7bc4009be0d9fbf413f4d20
Valeria Stamenova <stam0012@hz.nl>
2023-09-11 20:44:28 +0200

    Fix routing issue to myconference

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard/announcements';
+    public const HOME = '/myconference';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
