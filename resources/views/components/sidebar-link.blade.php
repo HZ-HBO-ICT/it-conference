2000028e7731e616326de32dc45a6fdaf2034e6e
Tim Kardol <tnmkardol@outlook.com>
2023-09-11 21:05:30 +0200

    Change dynamic style classes

--- resources/views/components/sidebar-link.blade.php
+++ resources/views/components/sidebar-link.blade.php
@@ -4,13 +4,13 @@
         (request()->route()->parameter('type') == $param || request()->route()->parameter('team') == $param)
     );
 
-    $accentColor = \Illuminate\Support\Facades\Auth::user()->current_team_id ? 'purple' : 'orange';
-    $bgColorClass = $isCurrentRoute ? "bg-gray-200 text-{$accentColor}-500 dark:bg-gray-700" : '';
+    $bgColorClass = $isCurrentRoute ? 'bg-gray-200 text-' . $roleColour . '-500 dark:text-' . $roleColour . '-500 dark:bg-gray-700 ' : '';
 @endphp
 <!-- Leave this to fool Tailwind compilation, otherwise it will delete dynamic styles. There is a better fix! -->
-<!-- Potential dynamic classes: text-crew-400 text-participant-400 text-partner-400 stroke-crew-500 stroke-participant-500 stroke-partner-500 -->
-<!-- dark:text-crew dark:text-participant dark:text-partner hover:text-crew hover:text-participant hover:text-partner dark:hover:text-crew dark:hover:text-participant dark:hover:text-partner-->
+<!-- Potential dynamic classes: text-crew-500 text-participant-500 text-partner-500 stroke-crew-500 stroke-participant-500 stroke-partner-500 -->
+<!-- dark:text-crew-500 dark:text-participant-500 dark:text-partner-500 hover:text-crew-500 hover:text-participant-500 hover:text-partner-500 dark:hover:text-crew-500 dark:hover:text-participant-500 dark:hover:text-partner-500 -->
 
+<!-- TODO/FIX: In dark mode the active link color does not work -->
 <li class="m-1">
     <a href="{{ empty($param) ? route($route) : route($route, $param) }}"
        class="leading-6 font-semibold text-sm p-2 rounded-md gap-x-3 flex hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-{{ $roleColour }}-500 dark:hover:text-{{ $roleColour }}-500 dark:text-white {{ $bgColorClass }}">
