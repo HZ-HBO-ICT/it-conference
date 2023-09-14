600c036e7188b35dea7287a2c002b5f14f1e1007
TimKardol <tnmkardol@outlook.com>
2023-09-14 10:06:30 +0200

    Fix tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,16 +1,28 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
-use PHPUnit\Framework\TestCase;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\SponsorTier;
+use App\Models\Team;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    public function test_the_application_returns_a_successful_response(): void
     {
-        $this->assertTrue(true);
+        SponsorTier::create([
+            'name' => 'golden',
+            'max_sponsors' => 1
+        ]);
+        Team::factory()->create(['sponsor_tier_id' => SponsorTier::where('name', 'golden')->first()->id]);
+        $route = route('welcome');
+
+        $response = $this->get($route);
+
+        $response->assertStatus(200);
     }
 }
