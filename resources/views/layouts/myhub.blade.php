b4a250b8631de639a448268ff7515f5ff8ebd898
Valeria Stamenova <stam0012@hz.nl>
2023-09-14 15:21:00 +0200

    Make view page for presentation accessible

--- resources/views/layouts/myhub.blade.php
+++ resources/views/layouts/myhub.blade.php
@@ -59,24 +59,47 @@
                                             :roleColour="'partner'"/>
                                         @endunlessrole
                                     @endif
+                                    @if(Auth::user()->speaker->presentation_id)
+                                        <x-sidebar-link
+                                            :type="'link'"
+                                            :label="'View presentation'"
+                                            :route="'presentations.show'"
+                                            :param="Auth::user()->speaker->presentation_id"
+                                            :icon="'M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6'"
+                                            :roleColour="'partner'"/>
+                                    @endif
                                 </ul>
                             </li>
                         @else
                             @if($canRequestPresentation)
-                            @unlessrole('content moderator')
+                        @unlessrole('content moderator')
+                            <li>
+                                <ul class="-mx-2" role="list">
+                                    <x-sidebar-link
+                                        :type="'link'"
+                                        :label="'Request presentation'"
+                                        :route="'speakers.request.presentation'"
+                                        :icon="'M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6'"
+                                        :roleColour="'partner'"/>
+                                </ul>
+                            </li>
+                        @endunlessrole
+                            @endif
+                            @if(Auth::user()->speaker)
                                 <li>
                                     <ul class="-mx-2" role="list">
                                         <x-sidebar-link
                                             :type="'link'"
-                                            :label="'Request presentation'"
-                                            :route="'speakers.request.presentation'"
+                                            :label="'View presentation'"
+                                            :route="'presentations.show'"
+                                            :param="Auth::user()->speaker->presentation_id"
                                             :icon="'M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6'"
                                             :roleColour="'partner'"/>
                                     </ul>
                                 </li>
-                            @endunlessrole
                             @endif
                         @endif
+
 
                         <!-- Profile Links -->
                         <li>
