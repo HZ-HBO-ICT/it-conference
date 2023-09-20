71b9efda20a713f1ebab7596d6feea78533d1a25
Daan de Waard <daan.de.waard@hz.nl>
2023-09-20 08:34:28 +0200

    Start redesign of my presentation

--- resources/views/livewire/upload-presentation.blade.php
+++ resources/views/livewire/upload-presentation.blade.php
@@ -2,43 +2,74 @@
     use Illuminate\Support\Facades\Auth;
     use App\Models\EventInstance;
 @endphp
-<div>
-    <form wire:submit.prevent="save" class="space-y-8">
+<x-form-section submit="save">
+    <x-slot name="title">
+        {{ __('Presentation file') }}
+    </x-slot>
+
+    <x-slot name="description">
+        {{ __('Update your presentation handout file.') }}
+    </x-slot>
+
+    <x-slot name="form">
+        <div class="col-span-6 sm:col-span-4">
+            @if ($presentation->file_path)
+                <x-label for="preview" value="{{ __('Current presentation') }}" class="pt-3 pb-1"/>
+                <p wire:click="downloadFile" style="cursor: pointer;">
+                    <svg class="h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
+                         aria-hidden="true">
+                        <path fill-rule="evenodd"
+                              d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                    <span class="text-blue-600">
+                                {{ basename($presentation->file_path) }}
+                            </span>
+                </p>
+                @if (session()->has('message'))
+                    <div class="text-sm text-green-600">
+                        {{ session('message') }}
+                    </div>
+                @endif
+            @endif
+            @if($presentation->file_path)
+                <div class="ml-4 flex min-w-0 flex-1 gap-2">
+                    <span class="truncate font-medium">{{$presentation->file_path}}</span>
+                </div>
+                <div class="ml-4 flex-shrink-0">
+                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+                </div>
+            @else
+                <span class="truncate font-medium">
+                                    {{ __('No presentation uploaded') }}
+                                </span>
+            @endif
+        </div>
+
+        <div class="col-span-6 sm:col-span-4">
+            <input type="file" id="file" wire:model="file"
+                   class="hidden">
+            <label for="file"
+                   class="flex items-center justify-center">
+                <x-secondary-button class="mt-2 mr-2" type="button" x-on:click.prevent="submit">
+                    {{ __('Select A New Presentation') }}
+                </x-secondary-button>
+            </label>
+
+
+            @if ($presentation->file_path)
+                <x-secondary-button type="button" class="mt-2" wire:click="">
+                    {{ __('Remove Presentation') }}
+                </x-secondary-button>
+            @endif
+
         <div class="space-y-8 divide-y divide-gray-200">
             <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
                 <div class="sm:col-span-6">
                     <x-label for="photo" value="{{ __('Presentation') }}"/>
-                    @if ($presentation->file_path)
-                        <x-label for="preview" value="{{ __('Current presentation') }}" class="pt-3 pb-1"/>
-                        <p wire:click="downloadFile" style="cursor: pointer;">
-                            <span class="text-blue-600">
-                                {{ basename($presentation->file_path) }}
-                            </span>
-                        </p>
-                        @if (session()->has('message'))
-                            <div class="text-sm text-green-600">
-                                {{ session('message') }}
-                            </div>
-                        @endif
-                    @endif
                     @if(!Auth::user()->hasRole('content moderator') && !EventInstance::current()->is_final_programme_released)
                         @if(Auth::user()->speaker->presentation_id == $presentation->id)
                             <div class="mt-1 flex items-center">
-                                <input type="file" id="file" wire:model="file"
-                                       class="hidden">
-                                <label for="file"
-                                       class="flex items-center justify-center w-1/3 h-10 px-4 mt-2 text-sm font-medium text-white bg-indigo-600 rounded-md cursor-pointer hover:bg-indigo-700 focus-within:bg-indigo-700">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke="currentColor" class="w-6 h-6 mr-2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                              d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-                                    </svg>
-                                    @if($presentation->file_path)
-                                        Change file
-                                    @else
-                                        Upload file
-                                    @endif
-                                </label>
                             </div>
                         @endif
                     @endif
@@ -61,5 +92,15 @@
                 </div>
             @endif
         @endif
-    </form>
-</div>
+    </x-slot>
+
+    <x-slot name="actions">
+        <x-action-message class="mr-3" on="saved">
+            {{ __('Saved.') }}
+        </x-action-message>
+
+        <x-button wire:loading.attr="disabled" wire:target="photo">
+            {{ __('Save') }}
+        </x-button>
+    </x-slot>
+</x-form-section>
--- resources/views/speakers/presentation/show.blade.php
+++ resources/views/speakers/presentation/show.blade.php
@@ -2,47 +2,74 @@
     use Carbon\Carbon;
     use App\Models\EventInstance;
 @endphp
-
+@php
+    use Illuminate\Support\Facades\Auth;
+@endphp
 <x-hub-layout>
-    <x-presentation-details
-        :presentation="$presentation"
-        :presentationName="$presentation->name"
-        :presentationDescription="$presentation->description"
-        :filename="basename($presentation->file_path)"
-        :presentationType="$presentation->type"
-        :presentationMaxParticipants="$presentation->max_participants"
-    />
+    <div class="py-8 px-8 mx-auto max-w-7xl">
+        <h2 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('My presentation') }}
+        </h2>
+        <div class="pt-5">
+            <x-action-section>
+                <x-slot name="title">
+                    {{ __('Presentation information') }}
+                </x-slot>
 
-    <?php /*
-    <!-- <h1 class="text-4xl font-extrabold text-gray-700 dark:text-white ml-4 py-5">Presentation details</h1>
-    <div class="grid grid-cols-2 gap-4 text-gray-900 dark:text-gray-200">
-        <div class="pl-4">
-            <h3 class="text-xl font-extrabold text-gray-700 dark:text-white ml-4 py-5">Details provided by you</h3>
-            <p class="text-lg pb-2">Title of presentation: {{$presentation->name}} </p>
-            <p class="text-lg">Description of the presentation:</p>
-            <p class="text-lg">{{$presentation->description}}</p>
-            <p class="text-lg py-2">Type: {{ucfirst($presentation->type)}} </p>
-            <p class="text-lg py-2">Max participants that you wish: {{$presentation->max_participants}} </p>
-            </h2>
+                <x-slot name="description">
+                    {{ __('The detail information about your presentation.') }}
+                </x-slot>
+
+                <x-slot name="content">
+                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Presentation title</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{$presentation->name}}</dd>
+                    </div>
+                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Presentation description</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{$presentation-> description}}</dd>
+                    </div>
+                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Presentation type</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{$presentation->type}}</dd>
+                    </div>
+                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Suggested max participants</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{$presentation->max_participants}}</dd>
+                    </div>
+                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Presentation status</dt>
+                        <dd class="mt-1 text-sm leading-6 {{$presentation->mainSpeaker()->is_approved ? "text-green-500" : "text-yellow-500"}} sm:col-span-2 sm:mt-0">
+                            {{$presentation->mainSpeaker()->is_approved ? 'Approved' : 'Awaiting approval'}}
+                        </dd>
+                    </div>
+                    <div class="mt-5">
+                        <x-button type="button" wire:loading.attr="disabled">
+                            {{ __('Edit') }}
+                        </x-button>
+
+                    </div>
+                </x-slot>
+
+            </x-action-section>
+
             <x-section-border/>
-            <h3 class="text-xl font-extrabold text-gray-700 dark:text-white ml-4 py-5">Presentation slides</h3>
+
             @livewire('upload-presentation', ['presentation' => $presentation])
         </div>
-        <div>
-            <div>
-                <h3 class="text-2xl font-semibold pb-3">Schedule</h3> -->
-    <!-- @if(EventInstance::current()->is_final_programme_released)
-        <p class="text-lg">Time: {{Carbon::parse($presentation->timeslot->start)->format('H:i')}}
-        - {{(Carbon::parse($presentation->timeslot->start)->addMinutes(30))->format('H:i')}}</p>
-                    <p class="text-lg py-2">Room: {{$presentation->room->name}} </p>
-                    <p class="text-lg py-2">Participants: {{$presentation->maxParticipants()}} </p>
-
-    @else
-        <p class="text-lg">The programme hasn't been released yet</p>
-
-    @endif -->
-    <!-- </div>
-</div>
-</div> -->
-*/ ?>
+    </div>
 </x-hub-layout>
+
+
+
+
+
+{{--<x-hub-layout>--}}
+{{--    <x-presentation-details--}}
+{{--        :presentation="$presentation"--}}
+{{--        :presentationName="$presentation->name"--}}
+{{--        :presentationDescription="$presentation->description"--}}
+{{--        :filename="basename($presentation->file_path)"--}}
+{{--        :presentationType="$presentation->type"--}}
+{{--        :presentationMaxParticipants="$presentation->max_participants"--}}
+{{--    />--}}
