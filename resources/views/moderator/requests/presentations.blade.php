9011b62d80ed6ca7e46c35df82ac1a1e6205f155
Tim Kardol <tnmkardol@outlook.com>
2023-09-26 20:35:32 +0200

    Fix wrong icon

--- resources/views/moderator/requests/presentations.blade.php
+++ resources/views/moderator/requests/presentations.blade.php
@@ -22,7 +22,7 @@
                                                 <div class="flex">
                                                     <div class="text-gray-700 dark:text-white text-m items-center flex">
                                                         <svg class="shrink-0 w-6 h-6 mr-1.5 block stroke-crew-400" xlmns="http://www.w3.org/2000/svg" viewbox="0 0 23 23" fill="none" aria-hidden="true">
-                                                            <path d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"></path>
+                                                            <path d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"></path>
                                                         </svg>
                                                         {{$presentation->name}}
                                                     </div>
