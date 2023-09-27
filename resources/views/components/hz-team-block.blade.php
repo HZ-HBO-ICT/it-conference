4f6b7a520ca67dc3994405190b7c36ae5d3363aa
Valeria Stamenova <stam0012@hz.nl>
2023-09-27 22:02:55 +0200

    Fix join presentations part of the hz component

--- resources/views/components/hz-team-block.blade.php
+++ resources/views/components/hz-team-block.blade.php
@@ -58,48 +58,50 @@
             </div>
         </dl>
     @endif
-    @can('request', App\Models\Presentation::class)
-        <dl class="pt-3 pb-5 px-6">
-            <div
-                class="py-5 px-4 rounded-lg overflow-hidden relative bg-sky-100 dark:bg-sky-900 shadow-md dark:shadow-md">
-                <dt>
-                    <div class="p-3 rounded-md absolute bg-sky-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             stroke-width="1.5" stroke="white"
-                             aria-hidden="true" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"/>
-                        </svg>
-                    </div>
-                    <p class="ml-16 pt-3 font-semibold text-md text-gray-500 dark:text-gray-100 overflow-hidden text-ellipsis">
-                        As a member of the HZ team you can <a>request a presentation</a> or join one of the
-                        presentations
-                        shown below.
-                    </p>
-                </dt>
-                <dd class="pt-4">
-                    <div class="grid place-items-center gap-3">
-                        @if(Auth::user()->currentTeam->allPresentations->count() > 0)
-                            @foreach(Auth::user()->currentTeam->allPresentations->unique() as $index => $presentation)
-                                <form action="{{route('cohost.presentation', $presentation)}}" method="POST">
-                                    @csrf
-                                    <button
-                                        class="flex items-center bg-gray-100 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-900 text-gray-600 dark:text-white font-semibold justify-center py-3 px-6 w-full rounded-lg transition duration-300 ease-in-out break-all">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                             stroke="currentColor"
-                                             class="w-6 h-6 mr-2">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                  d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"/>
-                                            </path>
-                                        </svg>
-                                        <span>Join {{$presentation->mainSpeaker()->user->name}} as a co-speaker in their presentation named {{$presentation->name}}</span>
-                                    </button>
-                                </form>
-                            @endforeach
-                        @endif
-                    </div>
-                </dd>
-            </div>
-        </dl>
-    @endcan
+    @if(Auth::user()->currentTeam->allPresentations->count() > 0)
+        @can('request', App\Models\Presentation::class)
+            <dl class="pt-3 pb-5 px-6">
+                <div
+                    class="py-5 px-4 rounded-lg overflow-hidden relative bg-sky-100 dark:bg-sky-900 shadow-md dark:shadow-md">
+                    <dt>
+                        <div class="p-3 rounded-md absolute bg-sky-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke-width="1.5" stroke="white"
+                                 aria-hidden="true" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"/>
+                            </svg>
+                        </div>
+                        <p class="ml-16 pt-3 font-semibold text-md text-gray-500 dark:text-gray-100 overflow-hidden text-ellipsis">
+                            As a member of the HZ team you can <a class="underline" href="{{route('speakers.request.presentation')}}">request a presentation</a> or join one of the
+                            presentations
+                            shown below.
+                        </p>
+                    </dt>
+                    <dd class="pt-4">
+                        <div class="grid place-items-center gap-3">
+                            @if(Auth::user()->currentTeam->allPresentations->count() > 0)
+                                @foreach(Auth::user()->currentTeam->allPresentations->unique() as $index => $presentation)
+                                    <form action="{{route('cohost.presentation', $presentation)}}" method="POST">
+                                        @csrf
+                                        <button
+                                            class="flex items-center bg-gray-100 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-900 text-gray-600 dark:text-white font-semibold justify-center py-3 px-6 w-full rounded-lg transition duration-300 ease-in-out break-all">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                 stroke="currentColor"
+                                                 class="w-6 h-6 mr-2">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                      d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"/>
+                                                </path>
+                                            </svg>
+                                            <span>Join {{$presentation->mainSpeaker()->user->name}} as a co-speaker in their presentation named {{$presentation->name}}</span>
+                                        </button>
+                                    </form>
+                                @endforeach
+                            @endif
+                        </div>
+                    </dd>
+                </div>
+            </dl>
+        @endcan
+    @endif
 </div>
