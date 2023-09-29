aa160e4105c1ccf44841d6c06f6ea75094d1b0cd
TimKardol <tnmkardol@outlook.com>
2023-09-29 12:26:17 +0200

    Add white background to sponsor cards on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,7 +90,7 @@
                 @if(!$allSponsors->isEmpty())
                     <div class="flex flex-wrap justify-center w-full gap-8 mt-16">
                         @foreach($allSponsors as $sponsor)
-                            <div class="border shadow-lg border-2 w-full @if($allSponsors->count() > 3) px-3 lg:w-1/3 xl:w-1/4 @endif @if ($sponsor->sponsorTier->name == 'golden' && $sponsor->is_approved) border-gold block
+                            <div class="bg-white border shadow-lg border-2 w-full @if($allSponsors->count() > 3) px-3 lg:w-1/3 xl:w-1/4 @endif @if ($sponsor->sponsorTier->name == 'golden' && $sponsor->is_approved) border-gold block
                             @elseif ($sponsor->sponsorTier->name == 'silver' && $sponsor->is_sponsor_approved) border-silver block
                             @elseif ($sponsor->sponsorTier->name == 'bronze' && $sponsor->is_sponsor_approved) border-bronze hidden xl:block @endif rounded-lg">
                                 <a href="{{ $sponsor->website }}" target="_blank">
