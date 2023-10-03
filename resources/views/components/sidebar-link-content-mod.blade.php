66832e79046e306d9ca9b424b85856b59a61ab39
TimKardol <tnmkardol@outlook.com>
2023-10-03 21:51:44 +0200

    Change icon for Rooms link

--- resources/views/components/sidebar-link-content-mod.blade.php
+++ resources/views/components/sidebar-link-content-mod.blade.php
@@ -27,11 +27,13 @@
         @if($label == 'Edit profile')
         <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
         @endif
+        @if($label == 'Rooms')
+        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+        @endif
         </svg>
 
        <span>
            {{$label}}
         </span>
-</span>
     </a>
 </li>
