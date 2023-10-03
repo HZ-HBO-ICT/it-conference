6afb6d77e3043b5c6cc3094b49fef2a82e9afc5c
Ihor Novikov <novi0003@hz.nl>
2023-10-03 13:36:17 +0200

    added borders around logos

--- resources/views/companies.blade.php
+++ resources/views/companies.blade.php
@@ -13,7 +13,10 @@
                     @elseif ($team->sponsor_tier_id === 3 && $team->is_sponsor_approved === 1) border-bronze dark:border-bronze
                     @else border-gray-200 dark:border-gray-500 @endif">
                         @if($team->logo_path)
-                            <img class="object-scale-down w-56 h-56 mx-auto my-auto max-w-full block dark:text-white"
+                            <img class="object-scale-down rounded-full border-2 p-2 @if ($team->sponsor_tier_id === 1 && $team->is_sponsor_approved === 1) border-gold dark:border-gold
+                            @elseif ($team->sponsor_tier_id === 2 && $team->is_sponsor_approved === 1) border-silver dark:border-silver
+                            @elseif ($team->sponsor_tier_id === 3 && $team->is_sponsor_approved === 1) border-bronze dark:border-bronze
+                            @else border-gray-200 dark:border-gray-500 @endif w-56 h-56 mx-auto my-auto max-w-full block dark:text-white"
                                  src="{{ url('storage/'. $team->logo_path) }}" alt="Logo of {{$team->name}}">
                         @else
                             @php
