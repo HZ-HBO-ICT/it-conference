4b8dff26945845512f01f8bb371f1bcab31134fe
Valeria Stamenova <stam0012@hz.nl>
2023-10-07 19:46:51 +0200

    Create policy for deleting user [WIP]

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, User $model): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, User $model): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, User $model): bool
+    {
+        // If the auth user is content moderator they can always delete the user unless it's themselves
+        if ($user->hasRole('content moderator') && $user->id != $model->id)
+            return true;
+
+        // If the auth user want to remove their own profile
+        if ($user->id == $model->id) {
+            if ($user->speaker)
+                return !$user->speaker->is_approved; // If they are not approved as a speaker already, they can delete their account
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, User $model): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, User $model): bool
+    {
+        //
+    }
+}
