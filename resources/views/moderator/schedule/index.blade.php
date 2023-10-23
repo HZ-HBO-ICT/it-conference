665156661d59c3e17a2f50dbfeb5f7c856fe508c
Valeria Stamenova <stam0012@hz.nl>
2023-10-23 22:23:23 +0200

    Add to schedule overview adding opening and closing [WIP]

--- resources/views/moderator/schedule/index.blade.php
+++ resources/views/moderator/schedule/index.blade.php
@@ -1,4 +1,8 @@
 @php use Carbon\Carbon; @endphp
+@php
+    $anyScheduledLectures = false;
+    $anyScheduledWorkshops = false;
+@endphp
 <x-hub-layout>
     <div class="py-8 px-8 mx-auto max-w-7xl">
         <h1 class="font-semibold text-3xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -64,70 +68,90 @@
                     {{ __('Current version of schedule') }}
                 </h2>
                 <div class="px-4 py-5 sm:p-6 bg-white dark:bg-gray-800 shadow sm:rounded-md">
-                    <div class="grid grid-cols-2 gap-4">
-                        <div>
-                            <table class="table-auto w-full text-gray-900 dark:text-gray-200">
-                                <thead>
-                                <tr class="text-left">
-                                    <th>Timeframe</th>
-                                    <th>Lectures</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($lectureTimeslots as $timeslot)
-                                    @if($timeslot->presentations->count() > 0)
-                                        <tr>
-                                            <td class="text-left text-gray-900 dark:text-white align-top">
-                                                {{Carbon::parse($timeslot->start)->format('H:i')}}
-                                                - {{(Carbon::parse($timeslot->start)->addMinutes(30))->format('H:i')}}
-                                            </td>
-                                            <td class="pb-3">
-                                                @foreach($timeslot->presentations as $lecture)
-                                                    @if($lecture->timeslot_id == $timeslot->id)
-                                                        <a href="{{route('moderator.schedule.presentation', $lecture)}}">
-                                                            <x-schedule-block :presentation="$lecture"/>
-                                                        </a>
-                                                    @endif
-                                                @endforeach
-                                            </td>
-                                        </tr>
-                                    @endif
-                                @endforeach
-                                </tbody>
-                            </table>
+                    @if(\App\Models\Timeslot::all()->count() == 0)
+                        <div class="text-center font-italic text-lg text-gray-800 dark:text-gray-200 leading-tight">
+                            There are no timeslots generated yet. <a
+                                href="{{route('moderator.schedule.timeslots.create')}}"
+                                class="underline text-crew-500">Generate timeslots first</a>
                         </div>
-                        <div>
-                            <table class="table-auto w-full text-gray-900 dark:text-gray-200">
-                                <thead>
-                                <tr class="text-left">
-                                    <th>Timeframe</th>
-                                    <th>Workshops</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($workshopTimeslots as $timeslot)
-                                    @if($timeslot->presentations->count() > 0)
-                                        <tr>
-                                            <td class="text-left text-gray-900 dark:text-white align-top">
-                                                {{Carbon::parse($timeslot->start)->format('H:i')}}
-                                                - {{(Carbon::parse($timeslot->start)->addMinutes(90))->format('H:i')}}
-                                            </td>
-                                            <td class="pb-3">
-                                                @foreach($timeslot->presentations as $workshop)
-                                                    @if($workshop->timeslot_id == $timeslot->id)
-                                                        <a href="{{route('moderator.schedule.presentation', $workshop)}}">
-                                                            <x-schedule-block :presentation="$workshop"/>
-                                                        </a>
-                                                    @endif
-                                                @endforeach
-                                            </td>
-                                        </tr>
-                                    @endif
-                                @endforeach
-                                </tbody>
-                            </table>
+                    @else
+                        <div class="grid grid-cols-2 gap-4">
+                            <div>
+                                <table class="table-auto w-full text-gray-900 dark:text-gray-200">
+                                    <thead>
+                                    <tr class="text-left">
+                                        <th>Timeframe</th>
+                                        <th>Lectures</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($lectureTimeslots as $timeslot)
+                                        @if($timeslot->presentations->count() > 0)
+                                            <tr>
+                                                <td class="text-left text-gray-900 dark:text-white align-top">
+                                                    {{Carbon::parse($timeslot->start)->format('H:i')}}
+                                                    - {{(Carbon::parse($timeslot->start)->addMinutes(30))->format('H:i')}}
+                                                </td>
+                                                <td class="pb-3">
+                                                    @foreach($timeslot->presentations as $lecture)
+                                                        @if($lecture->timeslot_id == $timeslot->id)
+                                                            <a href="{{route('moderator.schedule.presentation', $lecture)}}">
+                                                                <x-schedule-block :presentation="$lecture"/>
+                                                            </a>
+                                                            @php
+                                                                $anyScheduledLectures = true
+                                                            @endphp
+                                                        @endif
+                                                    @endforeach
+                                                </td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div>
+                                <table class="table-auto w-full text-gray-900 dark:text-gray-200">
+                                    <thead>
+                                    <tr class="text-left">
+                                        <th>Timeframe</th>
+                                        <th>Workshops</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($workshopTimeslots as $timeslot)
+                                        @if($timeslot->presentations->count() > 0)
+                                            <tr>
+                                                <td class="text-left text-gray-900 dark:text-white align-top">
+                                                    {{Carbon::parse($timeslot->start)->format('H:i')}}
+                                                    - {{(Carbon::parse($timeslot->start)->addMinutes(90))->format('H:i')}}
+                                                </td>
+                                                <td class="pb-3">
+                                                    @foreach($timeslot->presentations as $workshop)
+                                                        @if($workshop->timeslot_id == $timeslot->id)
+                                                            <a href="{{route('moderator.schedule.presentation', $workshop)}}">
+                                                                <x-schedule-block :presentation="$workshop"/>
+                                                            </a>
+                                                            @php
+                                                                $anyScheduledWorkshops = true
+                                                            @endphp
+                                                        @endif
+                                                    @endforeach
+                                                </td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                    </div>
+                        @if(!$anyScheduledLectures && !$anyScheduledWorkshops)
+                            <div
+                                class="text-center  pt-5 font-italic text-lg text-gray-800 dark:text-gray-200 leading-tight">
+                                There are no presentation scheduled yet. Assign them manually or use the autofill
+                            </div>
+                        @endif
+                    @endif
                 </div>
             </div>
         </div>
