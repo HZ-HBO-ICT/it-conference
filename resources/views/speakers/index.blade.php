4ac717a10a16b124465d026f7701d1b36f3f0d30
Valeria Stamenova <stam0012@hz.nl>
2023-11-03 10:50:03 +0100

    Add keynote to speakers page

--- resources/views/speakers/index.blade.php
+++ resources/views/speakers/index.blade.php
@@ -10,6 +10,19 @@
                     <h2 class="tracking-tight leading-10 font-bold text-2xl dark:text-white">Speakers Line-up</h2>
                 </div>
                 <ul class="grid-cols-1 gap-y-5 md:gap-x-8 md:gap-y-8 md:grid-cols-3 max-w-none mx-0 grid" role="list">
+                    <li class="px-3 py-6 lg:px-10 lg:py-8 sm:col-span-3 grid grid-cols-3 rounded-2xl border-2 shadow dark:bg-gray-800 border-gray-200">
+                        <div>
+                            <img class="w-56 h-56 rounded-full mx-auto my-auto max-w-full block dark:text-white"
+                                 src="img/keynote.png" alt="Photo of ">
+                        </div>
+                        <div class="sm:col-span-2">
+                            <h3 class="tracking-tight leading-7 font-semibold text-xl mt-3 dark:text-white">Keynote Speaker - Jolanda ter Maten</h3>
+                            <p class="leading-6 text-sm mt-1 dark:text-gray-200">Jolanda ter Maten is an international trainer, consultant, author and speaker. In addition, she is regularly hired as an expert at the European Commission for "Blockchain Observatory & Forum" and "Horizon2020".
+                                                                                               It is her drive to help business leaders worldwide in the transition to a new digital society and has more than 30 years of experience in making the opportunities and possibilities that new technology offers understandable and insightful.
+                                                                                               She is the author of the book From Buzz to Bizz: Your strategic guide in a complex world of emerging technologies  in which she simplifies the complex subject matter of new technologies into understandable and easily digestible information
+                            </p>
+                        </div>
+                    </li>
                     @foreach ($speakers as $speaker)
                         <li class="px-3 py-6 lg:px-10 lg:py-8 rounded-2xl border-2 shadow dark:bg-gray-800 border-gray-200">
                             <img class="w-56 h-56 rounded-full mx-auto my-auto max-w-full block dark:text-white"
