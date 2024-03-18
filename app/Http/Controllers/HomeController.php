6c0b26966c98e78404e1696001f1ca819e0f5167
v-stamenova <valeria.stamenova3@gmail.com>
2024-03-18 15:36:44 +0100

    Add new code sniffer action

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         return view('welcome');
     }
 }
