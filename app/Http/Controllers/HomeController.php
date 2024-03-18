0e340cbf0f149e290d8bf5c4b6bd271d0fac5a0b
v-stamenova <valeria.stamenova3@gmail.com>
2024-03-18 15:20:35 +0100

    Specify in `main.yml` to check all .php files

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('welcome');
     }
 }
