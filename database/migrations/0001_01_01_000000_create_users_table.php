45e6e498145f416750ed428ee63bca9d23354c52
v-stamenova <valeria.stamenova3@gmail.com>
2024-03-26 09:34:06 +0100

    Add to the initial users migration institution and mail preference

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('institution')->nullable();
+            $table->tinyInteger('receive_emails')->nullable()->default(true);
             $table->timestamps();
         });
 
