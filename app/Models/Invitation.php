a1f4c6f227da09e8e8782f09c2891a0d67aa66cf
v-stamenova <valeria.stamenova3@gmail.com>
2024-03-26 09:19:54 +0100

    Create custom scopes for the different invitations types

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Query\Builder;
 
 class Invitation extends Model
 {
@@ -17,4 +18,27 @@ class Invitation extends Model
     public function company() : BelongsTo {
         return $this->belongsTo(Company::class);
     }
+
+    /**
+     * Scope a query to only retrieve the invitations for participants
+     * Usage: Invitation::participants()
+     * @param Builder $query
+     * @return void
+     */
+    public function scopeParticipants(Builder $query): void {
+        $query->whereNull('company_id')
+            ->where('role', '=', 'participant');
+    }
+
+
+    /**
+     * Scope a query to only retrieve the invitations for employees of companies
+     * Usage: Invitation::employees()
+     * @param Builder $query
+     * @return void
+     */
+    public function scopeEmployees(Builder $query) : void {
+        $query->whereNotNull('company_id');
+    }
+
 }
