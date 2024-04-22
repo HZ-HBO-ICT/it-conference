e960ff254468ecbc9e4f6e67f85cf1f19c3cbd43
TimKardol <tnmkardol@outlook.com>
2024-04-22 17:05:00 +0200

    Add map and marker to contact page

--- resources/js/mapBox.js
+++ resources/js/mapBox.js
@@ -4,7 +4,10 @@ mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
 
 const map = new mapboxgl.Map({
     container: 'map',
-    center: [],
-    style: 'mapbox://styles/mapbox/streets-v11'
+    center: [3.609426676346345, 51.49491334862108],
+    zoom: 16,
+    style: 'mapbox://styles/mapbox/streets-v12'
 });
+
+let marker = new mapboxgl.Marker().setLngLat([3.609426676346345, 51.49491334862108]).addTo(map);
 
