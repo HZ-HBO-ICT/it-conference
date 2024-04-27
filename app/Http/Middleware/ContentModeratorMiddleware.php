10b8f8ab3fe201383c1f58a513e1de047d9e3b35
Ihor Novikov <novi0003@hz.nl>
2024-04-27 18:57:44 +0200

    Added middleware for content moderator

---
+++ app/Http/Middleware/ContentModeratorMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ContentModeratorMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && auth()->user()->hasRole('content moderator')) {
+            return $next($request);
+        }
+
+        abort(403);
+    }
+}
