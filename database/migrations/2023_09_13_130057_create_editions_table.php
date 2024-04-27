7329b53533383a87c19d2158e2c677efe7c3e971
Ihor Novikov <novi0003@hz.nl>
2024-04-27 20:02:08 +0200

    Added `name` column to editions table

--- database/migrations/2023_09_13_130057_create_editions_table.php
+++ database/migrations/2023_09_13_130057_create_editions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('editions', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->integer('state')->default(Edition::STATE_DESIGN);
             $table->timestamp('start_at')->nullable();
             $table->timestamp('end_at')->nullable();
