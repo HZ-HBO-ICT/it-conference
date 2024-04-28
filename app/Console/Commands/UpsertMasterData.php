0241dc6db6d17b62c3f59b3448bf54c17d53e130
Ihor Novikov <novi0003@hz.nl>
2024-04-28 15:15:28 +0200

    Added default events to the command

--- app/Console/Commands/UpsertMasterData.php
+++ app/Console/Commands/UpsertMasterData.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Difficulty;
+use App\Models\Event;
 use App\Models\Sponsorship;
 use App\Models\SponsorTier;
 use Exception;
@@ -60,7 +61,21 @@ class UpsertMasterData extends Command
                 'level' => 'expert',
                 'description' => 'Challenge yourself with these advanced presentations'
             ]
-        ]
+        ],
+        Event::class => [
+            [
+                'id' => 1,
+                'name' => 'Company registration'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Participant registration'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Presentation request'
+            ]
+        ],
     ];
 
     /**
