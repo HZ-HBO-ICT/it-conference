b3c9e3e9de13e19656268a3f4a4d60047042ee24
Ihor Novikov <novi0003@hz.nl>
2024-05-08 09:43:38 +0200

    Added validation with start date of edition to edition_event edit modal

--- resources/views/livewire/edition-event/edit-edition-event-modal.blade.php
+++ resources/views/livewire/edition-event/edit-edition-event-modal.blade.php
@@ -40,6 +40,7 @@
                         type="datetime-local"
                         id="end_at"
                         min="{{ $editionEvent->start_at }}"
+                        max="{{ $edition->start_at }}"
                         wire:model="form.end_at"
                         class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:[color-scheme:dark] focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block"
                     />
