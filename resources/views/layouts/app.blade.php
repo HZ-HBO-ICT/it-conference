11bce920285888df848d94be6249cf74609be6e9
v-stamenova <valeria.stamenova3@gmail.com>
2024-05-12 23:29:34 +0200

    Install some packages that help with dropable objects

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -133,5 +133,6 @@
 @stack('scripts')
 @livewireScripts
 @livewire('wire-elements-modal')
+<script src="https://cdn.jsdelivr.net/gh/livewire/sortable@v1.x.x/dist/livewire-sortable.js"></script>
 </body>
 </html>
