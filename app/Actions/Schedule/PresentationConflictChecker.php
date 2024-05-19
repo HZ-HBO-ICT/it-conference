b85c5417b939c4b34d7884141081a2090005a5e0
v-stamenova <valeria.stamenova3@gmail.com>
2024-05-19 19:30:02 +0200

    Move the checks for conflicting presentations to a standalone action class

---
+++ app/Actions/Schedule/PresentationConflictChecker.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Actions\Schedule;
+
+use App\Models\Presentation;
+use Carbon\Carbon;
+
+class PresentationConflictChecker
+{
+    /**
+     * Based on the passed presentation, room and new start estimates if there
+     * are any conflict in the scheduling system
+     * @param $presentation
+     * @param $room
+     * @param $start
+     * @return bool
+     */
+    public function isClearOfConflicts($presentation, $room, $start): bool
+    {
+        $previousPresentation = $this->findConflictPresentationBefore($room, $start);
+        $nextPresentation = $this->findConflictPresentationAfter($room, $start, $presentation);
+
+        // Case 1: No presentations in the room before this one or after it that cause conflict
+        if (is_null($previousPresentation)
+            && is_null($nextPresentation)) {
+            return true;
+        }
+
+        // Case 2: Next presentation that causes conflict is actually the same presentation trying to be moved later
+        if (!is_null($nextPresentation) && $presentation->id == $nextPresentation->id && is_null($previousPresentation)) {
+            return true;
+        }
+
+        // Case 3: Previous presentation that causes conflict is
+        // actually the same presentation trying to be moved earlier
+        if (!is_null($previousPresentation) && $presentation->id == $previousPresentation->id && is_null($nextPresentation)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Finds the closest starting presentation before the suggested start time
+     * to check if a conflict occurs
+     * @param $room
+     * @param $suggestedStart
+     * @return mixed|null null if there is no conflict, otherwise the presentation that causes the conflict
+     */
+    protected function findConflictPresentationBefore($room, $suggestedStart): mixed
+    {
+        $presentationStartTime = Carbon::parse($suggestedStart);
+
+        $potentialConflict = $room->presentations()
+            ->where('start', '<', $presentationStartTime)
+            ->orderBy('start', 'desc')
+            ->first();
+
+        if (is_null($potentialConflict)) {
+            return null;
+        }
+
+        $duration = $potentialConflict->type == 'workshop' ?
+            Presentation::$WORKSHOP_DURATION :
+            Presentation::$LECTURE_DURATION;
+
+        return Carbon::parse($potentialConflict->start)->copy()->addMinutes($duration) <= $presentationStartTime
+            ? null
+            : $potentialConflict;
+    }
+
+    /**
+     * Finds the closest starting presentation after the suggested start time
+     * to check if a conflict occurs
+     * @param $room
+     * @param $suggestedStart
+     * @param $presentation
+     * @return mixed|null null if there is no conflict, otherwise the presentation that causes the conflict
+     */
+    protected function findConflictPresentationAfter($room, $suggestedStart, $presentation): mixed
+    {
+        $duration = $presentation->type == 'workshop' ?
+            Presentation::$WORKSHOP_DURATION :
+            Presentation::$LECTURE_DURATION;
+        $presentationStartTime = Carbon::parse($suggestedStart);
+        $presentationEndTime = $presentationStartTime->copy()->addMinutes($duration);
+
+        $potentialConflict = $room->presentations()
+            ->where('start', '>=', $presentationStartTime)
+            ->orderBy('start', 'asc')
+            ->first();
+
+        if (is_null($potentialConflict)) {
+            return null;
+        }
+
+        $duration = $potentialConflict->type == 'workshop' ?
+            Presentation::$WORKSHOP_DURATION :
+            Presentation::$LECTURE_DURATION;
+
+        return $presentationEndTime <= Carbon::parse($potentialConflict->start)
+            ? null
+            : $potentialConflict;
+    }
+}
--- app/Livewire/Schedule/Cell.php
+++ app/Livewire/Schedule/Cell.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Schedule;
 
+use App\Actions\Schedule\PresentationConflictChecker;
 use App\Models\Presentation;
 use App\Models\Room;
 use App\Models\Timeslot;
@@ -21,6 +22,7 @@ class Cell extends Component
         $this->timeslot = $timeslot;
         $this->room = $room;
         $this->id = "r-{$this->room->id}-t-{$this->timeslot->id}";
+
         $this->presentations = $this->room->presentations()
             ->where('start', '>=', Carbon::parse($this->timeslot->start))
             ->where('start', '<', Carbon::parse($this->timeslot->start)->copy()->addMinutes(30))
@@ -35,86 +37,15 @@ class Cell extends Component
             $room = Room::find($newRoom);
             $timeslot = Timeslot::find($newTimeslot);
 
-            if ($this->isClearOfConflicts($presentation, $room, $timeslot)) {
-                $this->dispatchMovingEventToGrid($presentation->id, $room->id, $timeslot->id, $timeslot->start);
+            $presentationChecker = new PresentationConflictChecker();
+            if ($presentationChecker->isClearOfConflicts($presentation, $room, $timeslot->start)) {
+                $this->dispatchMoveEventToGrid($presentation->id, $room->id, $timeslot->id, $timeslot->start);
             }
         }
     }
 
-    public function isClearOfConflicts($presentation, $room, $timeslot)
-    {
-        $previousPresentation = $this->findConflictPresentationBefore($room, $timeslot->start, $presentation);
-        $nextPresentation = $this->findConflictPresentationAfter($room, $timeslot->start, $presentation);
 
-        // Case 1: No presentations in the room before this one or after it that causes conflict
-        if (is_null($previousPresentation)
-            && is_null($nextPresentation)) {
-            return true;
-        }
-
-        // Case 2: Next presentation that causes conflict is actually the same presentation trying to be moved later
-        if (!is_null($nextPresentation) && $presentation->id == $nextPresentation->id && is_null($previousPresentation)) {
-            return true;
-        }
-
-        // Case 3: Previous presentation that causes conflict is
-        // actually the same presentation trying to be moved earlier
-        if (!is_null($previousPresentation) && $presentation->id == $previousPresentation->id && is_null($nextPresentation)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public function findConflictPresentationBefore($room, $suggestedStart, $presentation)
-    {
-        $presentationStartTime = Carbon::parse($suggestedStart);
-
-        $potentialConflict = $room->presentations()
-            ->where('start', '<', $presentationStartTime)
-            ->orderBy('start', 'desc')
-            ->first();
-
-        if (is_null($potentialConflict)) {
-            return null;
-        }
-
-        $duration = $potentialConflict->type == 'workshop' ?
-            Presentation::$WORKSHOP_DURATION :
-            Presentation::$LECTURE_DURATION;
-
-        return Carbon::parse($potentialConflict->start)->copy()->addMinutes($duration) <= $presentationStartTime
-            ? null
-            : $potentialConflict;
-    }
-
-    public function findConflictPresentationAfter($room, $suggestedStart, $presentation)
-    {
-        $duration = $presentation->type == 'workshop' ?
-            Presentation::$WORKSHOP_DURATION :
-            Presentation::$LECTURE_DURATION;
-        $presentationStartTime = Carbon::parse($suggestedStart);
-        $presentationEndTime = $presentationStartTime->copy()->addMinutes($duration);
-
-        $potentialConflict = $room->presentations()
-            ->where('start', '>=', $presentationStartTime)
-            ->orderBy('start', 'asc')
-            ->first();
-
-        if (is_null($potentialConflict)) {
-            return null;
-        }
-
-        $duration = $potentialConflict->type == 'workshop' ?
-            Presentation::$WORKSHOP_DURATION :
-            Presentation::$LECTURE_DURATION;
-
-        return $presentationEndTime <= Carbon::parse($potentialConflict->start)
-            ? null
-            : $potentialConflict;
-    }
-
-    public function dispatchMovingEventToGrid($id, $newRoom, $newTimeslot, $newTime)
+    public function dispatchMoveEventToGrid($id, $newRoom, $newTimeslot, $newTime)
     {
         $this->dispatch('move-presentation', data: [
             'presentation_id' => $id,
