bf7ed5493fb19bab8adc850af20496bfc9c662c0
v-stamenova <valeria.stamenova3@gmail.com>
2024-05-20 15:16:27 +0200

    Shorten details in presentation block if character count is exceeded

--- resources/views/livewire/schedule/presentation.blade.php
+++ resources/views/livewire/schedule/presentation.blade.php
@@ -12,10 +12,16 @@
     <div class="flex flex-col p-1">
         <div class="flex items-center">
             <div class="w-1 h-6 {{$colors}} mr-1"></div>
-            <h3>{{ $presentation->name }}</h3>
+            <h3>
+            {{ strlen($presentation->name) > 20
+                 ? substr($presentation->name, 0, 20) . '...'
+                 : $presentation->name }}
+            </h3>
         </div>
         <div class="mt-px ml-2 text-xs w-full">
-            {{$details}}
+            {{ strlen($details) > 30
+                 ? substr($details, 0, 30) . '...'
+                 : $details }}
         </div>
     </div>
 </div>
