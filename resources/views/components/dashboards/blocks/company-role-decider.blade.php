9a2c730f90aa109f496bd69ce4445ec73698bcd8
v-stamenova <valeria.stamenova3@gmail.com>
2024-06-25 11:54:03 +0200

    Fix POST request issue - change it to GEt

--- resources/views/components/dashboards/blocks/company-role-decider.blade.php
+++ resources/views/components/dashboards/blocks/company-role-decider.blade.php
@@ -34,19 +34,16 @@
                             <!-- As per current implementation, it's an array it must be a POST request;
                             refer the backend of the component for more-->
                             @if(is_array($route))
-                                <form method="POST" action="{{ route($route[0], $route[1]) }}">
-                                    @csrf
-                                    <button
-                                        class="flex w-full items-center bg-partner-500 hover:bg-partner-700 dark:bg-partner-600 dark:hover:bg-gray-900 text-gray-200 dark:text-white font-semibold justify-center py-2 px-4 w-3/4 rounded-lg transition duration-300 ease-in-out">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                             stroke="currentColor"
-                                             class="w-6 h-6 mr-2">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                  d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-                                        </svg>
-                                        <span>{{ $label }}</span>
-                                    </button>
-                                </form>
+                                <a href="{{ route($route[0], $route[1]) }}"
+                                   class="flex w-full items-center bg-partner-500 hover:bg-partner-700 dark:bg-partner-600 dark:hover:bg-gray-900 text-gray-200 dark:text-white font-semibold justify-center py-2 px-4 w-3/4 rounded-lg transition duration-300 ease-in-out">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke="currentColor"
+                                         class="w-6 h-6 mr-2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                              d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                                    </svg>
+                                    <span>{{ $label }}</span>
+                                </a>
                             @else
                                 <a href="{{ route($route) }}"
                                    class="flex w-full items-center bg-partner-500 hover:bg-partner-700 dark:bg-partner-600 dark:hover:bg-gray-900 text-gray-200 dark:text-white font-semibold justify-center py-2 px-4 w-3/4 rounded-lg transition duration-300 ease-in-out">
