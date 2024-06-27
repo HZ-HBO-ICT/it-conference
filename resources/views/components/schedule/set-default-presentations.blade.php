ac4d217a496a2e6de5fc83fd5f6c599790ae088d
v-stamenova <valeria.stamenova3@gmail.com>
2024-06-27 12:40:13 +0200

    Alter the default flow of the schedule and require opening and closing before scheduling presentations

---
+++ resources/views/components/schedule/set-default-presentations.blade.php
@@ -0,0 +1,30 @@
+@php
+    use \App\Models\DefaultPresentation;
+@endphp
+
+<div class="flex items-center justify-center h-screen">
+    <div class="text-center w-3/4 h-3/4">
+        <h2 class="text-3xl">Heads Up!</h2>
+        <p class='py-5'>Before continuing to the schedule for all presentations, we need to add an opener and a closer.
+                        We don't need all the details right away, but knowing their start and end times is essential.
+                        You can add them here!</p>
+        <div class="grid grid-cols-2 w-full">
+            @if(is_null(DefaultPresentation::opening()))
+                <div class="w-full">
+                    <button class="bg-crew-300 py-2 px-3 border border-crew-400 hover:bg-crew-400 rounded"
+                            onclick="Livewire.dispatch('openModal', { component: 'schedule.add-default-presentation', arguments: { type: 'opening' }})">
+                        Create opening presentation
+                    </button>
+                </div>
+            @endif
+            @if(is_null(DefaultPresentation::closing()))
+                <div class="w-full">
+                    <button class="bg-crew-300 py-2 px-3 border border-crew-400 hover:bg-crew-400 rounded"
+                            onclick="Livewire.dispatch('openModal', { component: 'schedule.add-default-presentation', arguments: { type: 'closing' }})">
+                        Create closing presentation
+                    </button>
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
--- resources/views/crew/schedule/index.blade.php
+++ resources/views/crew/schedule/index.blade.php
@@ -1,8 +1,15 @@
-@php use Carbon\Carbon; @endphp
+@php
+    use Carbon\Carbon;
+    use App\Models\DefaultPresentation;
+@endphp
 <x-hub-layout>
     <div class="py-8 px-8 mx-auto max-w-7xl">
         <h1 class="font-semibold text-3xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Schedule management') }}</h1>
-        <livewire:schedule.grid-parent-component/>
+        @if(is_null(DefaultPresentation::opening()) || is_null(DefaultPresentation::closing()))
+            <x-schedule.set-default-presentations/>
+        @else
+            <livewire:schedule.grid-parent-component/>
+        @endif
     </div>
 </x-hub-layout>
