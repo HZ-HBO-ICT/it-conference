812e0ab3344a3b25eaff1eefcdbd0b5f6b9e4f1e
Ihor Novikov <novi0003@hz.nl>
2024-06-30 16:00:54 +0200

    Fixed bug with editing events

--- resources/views/livewire/edition-event/edit-edition-event-modal.blade.php
+++ resources/views/livewire/edition-event/edit-edition-event-modal.blade.php
@@ -40,7 +40,7 @@
                         type="date"
                         id="end_at"
                         min="{{ $editionEvent->start_at }}"
-                        max="{{ $edition->start_at->format('Y-m-d') }}"
+                        max="@if($edition->start_at) {{ $edition->start_at->format('Y-m-d') }} @endif"
                         wire:model="form.end_at"
                         class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:[color-scheme:dark] focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block"
                     />
