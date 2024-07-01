2ae7c5ba56b186f81163193fda78c6645481167c
v-stamenova <valeria.stamenova3@gmail.com>
2024-07-01 11:43:52 +0200

    Create a form for the motivation in the wizard

---
+++ app/Livewire/Registration/CompanyMotivationForm.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Livewire\Registration;
+
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+class CompanyMotivationForm extends Component
+{
+    #[Validate(['required'])]
+    public $motivation;
+
+    public function render()
+    {
+        return view('livewire.registration.company-motivation-form');
+    }
+
+    /**
+     * Dispatches an event to the parent component to go back
+     * @return void
+     */
+    public function goBack()
+    {
+        $this->dispatch('go-back', formName: 'CompanyMotivationInfoForm');
+    }
+
+    /**
+     * Dispatches an event to the parent to go next
+     * @return void
+     */
+    public function goNext()
+    {
+        $this->validate();
+
+        $this->dispatch('go-next', formName: 'CompanyMotivationInfoForm', input: [
+            'company_motivation' => $this->motivation,
+        ]);
+    }
+
+}
