9b811ba1cff26ca0f6cc36782009c6453dd40727
Ihor Novikov <novi0003@hz.nl>
2024-07-03 16:15:32 +0200

    Fixed bug with null property in layout

--- resources/views/layouts/hub.blade.php
+++ resources/views/layouts/hub.blade.php
@@ -1,6 +1,7 @@
 @php
     use Illuminate\Support\Facades\Auth;
     use App\Models\Presentation;
+    use App\Models\Edition;
 @endphp
 
 <x-app-layout>
@@ -22,7 +23,7 @@
                                 :route="'dashboard'"
                                 :icon="'M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'"
                                 :roleColour="Auth::user()->role_colour"></x-sidebar-link-content-mod>
-                            @if(\App\Models\Edition::current()->is_final_programme_released)
+                            @if(Edition::current() && Edition::current()->is_final_programme_released)
                                 <x-sidebar-link-content-mod
                                     :label="'My programme'"
                                     :route="'my.programme'"
