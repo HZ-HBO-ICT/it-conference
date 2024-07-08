30a21cb6a0ce331f6a41abb7e6ece436f869853c
v-stamenova <valeria.stamenova3@gmail.com>
2024-07-08 09:54:09 +0200

    Fix the retrieve of number of speakers

--- resources/views/components/dashboards/crew.blade.php
+++ resources/views/components/dashboards/crew.blade.php
@@ -4,6 +4,7 @@
     use App\Models\Booth;
     use App\Models\Company;
     use App\Models\User;
+    use App\Models\UserPresentation;
 @endphp
 
 @push('scripts')
@@ -65,7 +66,7 @@
                         :styleMode="'alternate'"/>
                     <x-dashboards.blocks.crew
                         :label="'Speakers'"
-                        :count="User::role('speaker')->get()->count()"
+                        :count="UserPresentation::where('role', 'speaker')->count()"
                         :icon="'M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605'"
                         :route="'moderator.list'"
                         :param="'speakers'"
