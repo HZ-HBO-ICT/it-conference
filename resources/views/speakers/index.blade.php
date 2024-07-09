7139eefd435affc31ba500ee5a6d90affd463ace
Ihor Novikov <novi0003@hz.nl>
2024-07-09 12:57:32 +0200

    Fixed spacing

--- resources/views/speakers/index.blade.php
+++ resources/views/speakers/index.blade.php
@@ -12,13 +12,13 @@
             <div class="relative mb-12 min-h-full bg-white dark:bg-gray-800 rounded-xl shadow-xl overflow-hidden transform transition-all hover:bg-gray-100 dark:hover:bg-gray-900">
                 <div class="absolute top-0 left-0 w-full h-2 {{$borderColor}}"></div>
                 <div class="p-8 flex items-start">
-                    <div class="relative w-full h-full">
+                    <div class="relative w-60 h-60">
                         <div class="absolute inset-0 rounded-full opacity-75 blur-lg"></div>
                         <img class="relative h-60 w-60 object-scale-down  rounded-full object-cover border-4 border-white"
                              src="{{ url('storage/' . $edition->keynote_photo_path) }}"
                              alt="Profile picture of {{$edition->keynote_name}}">
                     </div>
-                    <div class="flex flex-col justify-center mt-4">
+                    <div class="flex flex-col justify-center mt-4 ml-8">
                         <h3 class="font-bold text-2xl text-gray-900 dark:text-white">{{$edition->keynote_name}}</h3>
                         <p class="mt-4 text-gray-600 dark:text-gray-400">{{strlen($edition->keynote_description) > 700 ? substr($edition->keynote_description, 0, 700) . '...' : $edition->keynote_description}}</p>
                     </div>
