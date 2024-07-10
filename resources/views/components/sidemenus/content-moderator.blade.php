40eddf7a7cde52b28ee9ee9dc82b12f5de16c1a4
Ihor Novikov <novi0003@hz.nl>
2024-07-10 22:55:28 +0200

    Added badge for edition on the side menu

--- resources/views/components/sidemenus/content-moderator.blade.php
+++ resources/views/components/sidemenus/content-moderator.blade.php
@@ -56,6 +56,7 @@
             :label="'Editions'"
             :route="'moderator.editions.index'"
             :icon="'M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z'"
-            :roleColour="Auth::user()->role_colour"></x-sidebar-link-content-mod>
+            :roleColour="Auth::user()->role_colour"
+            :badgeText="Edition::where('state', Edition::STATE_DESIGN)->count()"></x-sidebar-link-content-mod>
     </ul>
 </li>
