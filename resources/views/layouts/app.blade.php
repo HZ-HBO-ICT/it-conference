df2d825e766098823fa623cacced56aeaf38e9e9
TimKardol <tnmkardol@outlook.com>
2024-07-11 10:53:45 +0200

    Fix icons in the footer (#472)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,37 +95,22 @@
         <span class="text-sm text-gray-500 dark:text-gray-300 sm:text-center"> © 2024 IT Together Conference | Made by IT-Conference Website Team. All Rights Reserved.
         </span>
                     <div class="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
-                        <a href="#" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
-                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="currentColor"
-                                 viewBox="0 0 8 19">
-                                <path fill-rule="evenodd"
-                                      d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                                      clip-rule="evenodd"/>
-                            </svg>
-                            <span class="sr-only">Facebook page</span>
-                        </a>
-                        <a href="#" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
-                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="currentColor"
-                                 viewBox="0 0 21 16">
+                        <a href="https://www.linkedin.com/company/we-are-in-it-together-conference/" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
+                            <svg class="w-4 h-4 fill-blue-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                                 <path
-                                    d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z"/>
+                                    d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z" />
                             </svg>
-                            <span class="sr-only">Discord community</span>
+                            <span class="sr-only">Linkedin</span>
                         </a>
-                        <a href="#" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
-                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="currentColor"
-                                 viewBox="0 0 20 17">
-                                <path fill-rule="evenodd"
-                                      d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
-                                      clip-rule="evenodd"/>
+                        <a href="https://www.youtube.com/@weareinittogether" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
+                            <svg class="w-4 h-4 fill-red-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                <path
+                                    d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
                             </svg>
-                            <span class="sr-only">Twitter page</span>
+                            <span class="sr-only">Youtube</span>
                         </a>
-                        <a href="#" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
-                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        <a href="https://github.com/HZ-HBO-ICT/it-conference" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
+                            <svg class="w-4 h-4 fill-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                  fill="currentColor"
                                  viewBox="0 0 20 20">
                                 <path fill-rule="evenodd"
