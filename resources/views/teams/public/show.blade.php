a9b8fe63f5a7cf555b8450515819d6a0e175793e
v-stamenova <valeria.stamenova3@gmail.com>
2024-08-27 09:38:56 +0200

    Make sure that the factory seeded url matches the required one in the registration form

--- resources/views/teams/public/show.blade.php
+++ resources/views/teams/public/show.blade.php
@@ -73,7 +73,7 @@
 
                     <div class="mt-5">
                         <h3 class="text-2xl font-semibold">Visit Our Website</h3>
-                        <a href="{{ $company->website }}" class="{{$linkColor}} hover:underline">
+                        <a href="http://{{ $company->website }}" class="{{$linkColor}} hover:underline">
                             {{ $company->website }}
                         </a>
                     </div>
