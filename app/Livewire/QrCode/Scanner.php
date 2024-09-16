2fd91e92257e5a53586e0e7e5c59a47fb81b158b
IGORnvk <novi0003@hz.nl>
2024-09-16 22:52:59 +0200

    Add function docs and remove unused logic

--- app/Livewire/QrCode/Scanner.php
+++ app/Livewire/QrCode/Scanner.php
@@ -2,11 +2,17 @@
 
 namespace App\Livewire\QrCode;
 
+use Illuminate\View\View;
 use LivewireUI\Modal\ModalComponent;
 
 class Scanner extends ModalComponent
 {
-    public function render()
+    /**
+     * Render the component
+     *
+     * @return View
+     */
+    public function render(): View
     {
         $this->dispatch('enableScanner');
         return view('livewire.qr-code.scanner');
