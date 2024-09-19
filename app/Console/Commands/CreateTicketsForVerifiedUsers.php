67e0f718054b134c150508e6804bec371a5a62ed
IGORnvk <novi0003@hz.nl>
2024-09-19 23:25:00 +0200

    Add command to create tickets for verified users

--- app/Console/Commands/CreateTicketsForVerifiedUsers.php
+++ app/Console/Commands/CreateTicketsForVerifiedUsers.php
@@ -3,25 +3,24 @@
 namespace App\Console\Commands;
 
 use App\Models\User;
-use Illuminate\Auth\Events\Verified;
 use Illuminate\Console\Command;
 use Exception;
 
-class ConfirmAllEmails extends Command
+class CreateTicketsForVerifiedUsers extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'admin:confirm-all-emails';
+    protected $signature = 'admin:create-tickets-for-verified-users';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Marks the emails of all registered users as confirmed';
+    protected $description = 'Creates tickets for all the users who verified their emails and are not crew.';
 
     /**
      * Execute the console command.
@@ -29,15 +28,11 @@ class ConfirmAllEmails extends Command
     public function handle()
     {
         try {
-            User::all()->each(function ($user) {
-                $user->update([
-                    'email_verified_at' => now()->timestamp
-                ]);
-
+            foreach (User::verified()->get() as $user) {
                 $user->createTicket();
-            });
+            }
 
-            $this->info('All emails are marked as confirmed successfully');
+            $this->info('Tickets were created successfully.');
         } catch (Exception) {
             $this->error('The emails could not be marked as confirmed');
         }
