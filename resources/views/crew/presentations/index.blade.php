7377ba53bc89cc32d15917903324ee1762bdefe2
v-stamenova <valeria.stamenova3@gmail.com>
2024-10-16 09:55:56 +0200

    Change the layout of crew presentation index page

--- resources/views/crew/presentations/index.blade.php
+++ resources/views/crew/presentations/index.blade.php
@@ -11,47 +11,63 @@
                     </x-button-link>
                 </x-slot>
                 <x-slot name="content">
-                    <ul role="list">
+                    <table class="min-w-full divide-gray-200 dark:divide-gray-700">
+                        <thead>
+                        <tr>
+                            <th class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
+                                Presentation Name
+                            </th>
+                            <th class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
+                                Company
+                            </th>
+                            <th class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
+                                Created At
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody class="bg-white dark:bg-gray-800 divide-y-4 divide-white dark:divide-gray-700">
                         @forelse($presentations as $index => $presentation)
-                            <x-list-section-item class="{{ $presentation->is_approved ? 'border-transparent hover:bg-gray-100 ' : 'bg-apricot-peach-400 bg-opacity-30 dark:bg-opacity-20 border-apricot-peach-300 dark:border-apricot-peach-600 hover:bg-apricot-peach-200 ' }} border-l-4">
-                            <a href="{{route('moderator.presentations.show', $presentation)}}" class="block">
-                                    <div class="justify-between flex mt-2">
-                                        <div class="flex">
-                                            <div class="text-gray-700 dark:text-white text-m items-center flex">
-                                                <svg
-                                                    class="shrink-0 w-6 h-6 mr-1.5 block stroke-apricot-peach-300 {{ !$presentation->is_approved ? 'stroke-gray-900 dark:stroke-white' : 'stroke-apricot-peach-300' }}"
-                                                    xlmns="http://www.w3.org/2000/svg" viewbox="0 0 23 23" fill="none"
-                                                    aria-hidden="true">
-                                                    <path
-                                                        d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"></path>
-                                                </svg>
-                                                <div class="ml-2 flex-grow">
-                                                    <strong>{{$presentation->name}}</strong>
-                                                    <br/>
-                                                    <span
-                                                        class="text-sm text-gray-500">{{ $presentation->speakernames }}</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="text-sm items-center flex ml-2 dark:text-white">
-                                            <svg
-                                                class="shrink-0 w-6 h-6 mr-1.5 block stroke-apricot-peach-400 {{ !$presentation->is_approved ? 'stroke-gray-900 dark:stroke-white' : '' }}"
-                                                xlmns="http://www.w3.org/2000/svg" viewbox="0 0 23 23" fill="none"
-                                                aria-hidden="true">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                      d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                            </svg>
-                                            {{$presentation->created_at->format('d/m/Y')}}
+                            <tr class="{{ $presentation->is_approved ? 'hover:bg-gray-100 dark:hover:bg-gray-700' : 'bg-apricot-peach-400 bg-opacity-30 dark:bg-opacity-20 hover:bg-apricot-peach-200' }} cursor-pointer"
+                                onclick="window.location='{{ route('moderator.presentations.show', $presentation) }}'">
+                                <td class="px-4 py-4 whitespace-nowrap text-gray-700 dark:text-white">
+                                    <div class="flex items-center">
+                                        <svg
+                                            class="shrink-0 w-6 h-6 mr-1.5 block stroke-apricot-peach-300 {{ !$presentation->is_approved ? 'stroke-gray-900 dark:stroke-white' : 'stroke-apricot-peach-300' }}"
+                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23" fill="none"
+                                            aria-hidden="true">
+                                            <path
+                                                d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"></path>
+                                        </svg>
+                                        <div class="ml-2">
+                                            <strong>{{ $presentation->name }}</strong>
                                         </div>
                                     </div>
-                                </a>
-                            </x-list-section-item>
+                                </td>
+                                <td class="px-4 py-2 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
+                                    {{ optional($presentation->company)->name }}
+                                </td>
+                                <td class="px-4 py-2 whitespace-nowrap text-sm text-gray-700 dark:text-white">
+                                    <div class="flex items-center">
+                                        <svg
+                                            class="shrink-0 w-6 h-6 mr-1.5 block stroke-apricot-peach-400 {{ !$presentation->is_approved ? 'stroke-gray-900 dark:stroke-white' : '' }}"
+                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23" fill="none"
+                                            aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                  d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                        </svg>
+                                        {{ $presentation->created_at->format('d/m/Y') }}
+                                    </div>
+                                </td>
+                            </tr>
                         @empty
-                            <p class="text-crew-400 text-lg justify-center flex m-12">There are currently no new
-                                                                                      presentations waiting on
-                                                                                      approval.</p>
+                            <tr>
+                                <td colspan="3" class="px-4 py-2 text-center text-gray-500 dark:text-gray-300">
+                                    There are currently no new presentations waiting on approval.
+                                </td>
+                            </tr>
                         @endforelse
-                    </ul>
+                        </tbody>
+                    </table>
                     <div class="pt-2">
                         {{ $presentations->links() }}
                     </div>
