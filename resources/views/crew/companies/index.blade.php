7c8f445fe1490ddd62e6781b27b41032af72e50d
v-stamenova <valeria.stamenova3@gmail.com>
2024-10-16 10:09:56 +0200

    Change the layout of crew companies index page

--- resources/views/crew/companies/index.blade.php
+++ resources/views/crew/companies/index.blade.php
@@ -10,26 +10,39 @@
                     <x-button-link href="{{route('moderator.companies.create')}}">
                         {{ __('Invite a company') }}
                     </x-button-link>
-
-                    {{--<x-button-link href="{{route('teams.create')}}" class="ml-2">
-                        {{__('Create a company')}}
-                    </x-button-link>--}}
                 </x-slot>
 
                 <x-slot name="content">
+                    <table class="min-w-full divide-gray-200 dark:divide-gray-700">
+                        <thead>
+                        <tr>
+                            <th class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
+                                Company Name
+                            </th>
+                            <th class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
+                                Company Representative
+                            </th>
+                            <th class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
+                                Created At
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody class="bg-white dark:bg-gray-800 divide-y-4 divide-white dark:divide-gray-700">
                         @forelse($companies as $index => $company)
-                        <x-list-section-item class="{{ $company->is_approved ? 'border-transparent hover:bg-gray-100 ' : 'bg-apricot-peach-400 bg-opacity-30 dark:bg-opacity-20 border-apricot-peach-300 dark:border-apricot-peach-600 hover:bg-apricot-peach-200 ' }} border-l-4"
-                                :url="route('moderator.companies.show', $company)">
-                                <div class="justify-between flex my-0.5">
+                            <tr class="{{ $company->is_approved ? 'hover:bg-gray-100 dark:hover:bg-gray-700' : 'bg-apricot-peach-400 bg-opacity-30 dark:bg-opacity-20 hover:bg-apricot-peach-200' }} cursor-pointer"
+                                onclick="window.location='{{ route('moderator.companies.show', $company) }}'">
+                                <td class="px-4 py-4 whitespace-nowrap text-gray-700 dark:text-white">
                                     <div class="flex">
                                         <div class="text-gray-700 dark:text-white text-m items-center flex">
                                             @if($company->logo_path)
                                                 <img class="w-6 h-6 mx-auto my-auto max-w-full block dark:text-white"
-                                                     src="{{ url('storage/'. $company->logo_path) }}" alt="Logo of {{$company->name}}">
+                                                     src="{{ url('storage/'. $company->logo_path) }}"
+                                                     alt="Logo of {{$company->name}}">
                                             @else
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                      stroke-width="1"
-                                                     stroke="gray" aria-hidden="true" class="w-6 h-6 {{ !$company->is_approved ? 'stroke-gray-900 dark:stroke-white' : 'stroke-apricot-peach-300' }}">
+                                                     stroke="gray" aria-hidden="true"
+                                                     class="w-6 h-6 {{ !$company->is_approved ? 'stroke-gray-900 dark:stroke-white' : 'stroke-apricot-peach-300' }}">
                                                     <path stroke-linecap="round" stroke-linejoin="round"
                                                           d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"/>
                                                 </svg>
@@ -41,24 +54,36 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="text-sm items-center flex ml-2 dark:text-white">
-                                        <svg class="shrink-0 w-6 h-6 mr-1.5 block stroke-apricot-peach-400 {{ !$company->is_approved ? 'stroke-gray-900 dark:stroke-white' : '' }}" xlmns="http://www.w3.org/2000/svg" viewbox="0 0 23 23" fill="none" aria-hidden="true">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                </td>
+                                <td class="px-4 py-2 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
+                                    {{ optional($company->representative)->name }}
+                                </td>
+                                <td class="px-4 py-2 whitespace-nowrap text-sm text-gray-700 dark:text-white">
+                                    <div class="flex items-center">
+                                        <svg
+                                            class="shrink-0 w-6 h-6 mr-1.5 block stroke-apricot-peach-400 {{ !$company->is_approved ? 'stroke-gray-900 dark:stroke-white' : '' }}"
+                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23" fill="none"
+                                            aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                  d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                         </svg>
-                                        {{$company->created_at->format('d/m/Y')}}
+                                        {{ $company->created_at->format('d/m/Y') }}
                                     </div>
-                                </div>
-                            </x-list-section-item>
+                                </td>
+                            </tr>
                         @empty
-                            <p class="text-crew-400 text-lg justify-center flex m-12">There are currently no new presentations waiting on approval.</p>
+                            <tr>
+                                <td colspan="3" class="px-4 py-2 text-center text-gray-500 dark:text-gray-300">
+                                    There are currently no companies.
+                                </td>
+                            </tr>
                         @endforelse
-
+                        </tbody>
+                    </table>
                     <div class="pt-2">
                         {{ $companies->links() }}
                     </div>
-
                 </x-slot>
-
             </x-list-section>
         </div>
     </div>
