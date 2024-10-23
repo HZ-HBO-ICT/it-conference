6b0c85472e64829519d960670164585c94643620
Tim Kardol <54664131+TimKardol@users.noreply.github.com>
2024-10-23 10:36:49 +0200

    Revert "Revert "Feature - QR codes/ticket system""

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Crew\FrequentQuestionController;
 use App\Http\Controllers\Crew\RoomController;
 use App\Http\Controllers\Crew\ScheduleController;
 use App\Http\Controllers\Crew\SponsorshipController;
+use App\Http\Controllers\Crew\TicketController;
 use App\Http\Controllers\Crew\UserController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Hub\ParticipantController;
@@ -156,6 +157,8 @@ Route::get('/programme', [ProgrammeController::class, 'index'])
 Route::get('/programme/presentation/{presentation}', [ProgrammeController::class, 'show'])
     ->name('programme.presentation.show');
 
+
+
 // ===== Routes for crew =====
 Route::middleware([
     'auth:sanctum',
@@ -232,4 +235,6 @@ Route::middleware([
     Route::resource('/moderator/rooms', RoomController::class);
 
     Route::get('/moderator/users/{role?}', [UserController::class, 'index'])->name('users.index');
+
+    Route::get('/moderator/tickets', [TicketController::class, 'index'])->name('tickets.index');
 });
