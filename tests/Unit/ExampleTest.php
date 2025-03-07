1f1829bd9f93ec1039805fa2f1a68812b6e8c933
Valeria Stamenova <valeria.stamenova3@gmail.com>
2025-03-07 14:29:26 +0100

    Move the refresh database in the right ExampleTest

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -8,16 +8,6 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    use RefreshDatabase;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-        activity()->disableLogging();
-        Artisan::call('admin:upsert-master-data');
-        Artisan::call('admin:sync-permissions');
-    }
-
     /**
      * A basic test example.
      */
