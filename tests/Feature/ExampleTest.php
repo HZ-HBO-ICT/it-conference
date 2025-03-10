078dd55b89fb7facb7f07640d9520483be41a7c7
Valeria Stamenova <77020646+v-stamenova@users.noreply.github.com>
2025-03-10 11:19:04 +0100

    Technical Debt - Slow build pipeline (#627)
    * Add caching to composer and npm dependencies
    
    * Restore the mistake of deleting the .env copy command
    
    * Try to avoid using xdebug
    
    * Test testing times with paratest
    
    * Clean up the action
    
    * Remove any coverage for now
    
    * Try to run test in parallel via artisan
    
    * Force the database refresh on the example test
    
    * Call the setup function in the ExampleTest
    
    * Move the refresh database in the right ExampleTest
    
    * Fix the coverage
    
    * Remove unused imports in the `Unit/ExampleTest.php`

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,12 +2,23 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        activity()->disableLogging();
+        Artisan::call('admin:upsert-master-data');
+        Artisan::call('admin:sync-permissions');
+    }
+
     /**
      * A basic test example.
      */
