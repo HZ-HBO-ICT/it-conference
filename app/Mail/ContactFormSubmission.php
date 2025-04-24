ccfbd12e6e39d022dd8e430c7d9a5200d6b78cc5
NikolAlexandrova <n1kol1vay@gmail.com>
2025-04-24 09:45:00 +0200

    Fixed code quality issues

--- app/Mail/ContactFormSubmission.php
+++ app/Mail/ContactFormSubmission.php
@@ -10,14 +10,24 @@ class ContactFormSubmission extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public $data;
+    public array $data;
 
+    /**
+     * Create a new message instance.
+     *
+     * @param array<string, string> $data  An associative array with contact form input
+     */
     public function __construct(array $data)
     {
         $this->data = $data;
     }
 
-    public function build()
+    /**
+     * Build the contact form submission email.
+     *
+     * @return $this
+     */
+    public function build(): static
     {
         return $this->markdown('emails.contact-form-submission')
             ->subject('New Contact Form Submission - ' . $this->data['subject'])
