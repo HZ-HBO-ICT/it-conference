e9ea34904ba6566315f03b0841a0f9730a5c2a26
Tim Kardol <tnmkardol@outlook.com>
2025-07-10 10:58:14 +0200

    Add 402 error page

---
+++ resources/views/errors/402.blade.php
@@ -0,0 +1,7 @@
+@extends('errors::minimal')
+
+@section('code', '402')
+@section('message')
+    This content or feature is only available to users with a valid ticket or subscription. <br>
+    Please complete your payment or upgrade your access level to continue.
+@endsection
