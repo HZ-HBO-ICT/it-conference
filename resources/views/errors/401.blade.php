0cdd37b2df835511e5585ce0e381213a8549d5c8
Tim Kardol <tnmkardol@outlook.com>
2025-07-11 14:36:03 +0200

    Remove part of the text in 401 error page

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -4,5 +4,5 @@
 @section('message')
     You must be logged in to access this page. <br>
     Please <a class="text-waitt-pink-400 hover:text-waitt-pink-600 underline" href="/login">sign in</a> to your
-    account or contact the event administrator if you believe you received this message in error.
+    account.
 @endsection
