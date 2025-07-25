d3c28a1d8c8fb720c9672e43523e42180cc51a7a
Tim Kardol <tnmkardol@outlook.com>
2025-07-25 16:04:04 +0200

    Rewrite part of the email text for user invitation

--- resources/views/emails/user-invite.blade.php
+++ resources/views/emails/user-invite.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
-# You have been invited to join We are in IT together conference!
-Create an account and accept the invitation following the link below
+# You have been invited to join the We are in IT Together Conference!
+Create an account and accept the invitation following the link below.
 
 @component('mail::button', ['url' => $acceptUrl])
 Accept your invitation
@@ -8,15 +8,19 @@ Accept your invitation
 
 You will be registered as a participant by default. When the final program is released you can enrol in any of the lectures/workshops using your account.
 
-As soon as your account has been created, you can request to host a presentation. When requesting a presentation make sure to enter the necessary information such as: a summary of the topic which will be shown on the website and the level of difficulty intended for the participants. There are three difficulty levels:
+As soon as your account has been created, you can request to host a presentation. When requesting a presentation make sure to enter the following information:
 
-Beginner: No specific knowledge/skill necessary.
+- A summary of the topic which will be shown on the website
+- The level of difficulty intended for the participants.
+There are three difficulty levels:
 
-Intermediate: Basic level of knowledge/skill.
+    - Beginner: No specific knowledge/skill necessary.
 
-Advanced: Experienced level of knowledge/skill.
+    - Intermediate: Basic level of knowledge/skill.
+
+    - Advanced: Experienced level of knowledge/skill.
 
 Kind regards,
 
-We are in IT together conference team
+The Conference Team
 @endcomponent
