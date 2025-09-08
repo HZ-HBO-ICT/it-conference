119a8579d2953f4d9a22bfed1c9254ec285bbab2
Valeria Stamenova <valeria.stamenova3@gmail.com>
2025-09-08 09:38:59 +0200

    Redesign the presentations pages

--- resources/views/livewire/presentation/edit-presentation-modal.blade.php
+++ resources/views/livewire/presentation/edit-presentation-modal.blade.php
@@ -1,4 +1,4 @@
-<x-livewire-modal form-action="save">
+<x-waitt.livewire-modal form-action="save">
     <x-slot name="title" class="dark:bg-gray-900 border-gray-800">
         Edit presentation
     </x-slot>
@@ -11,28 +11,28 @@
         <div class="px-4 py-6 sm:px-0">
             <dl class="sm:grid sm:grid-cols-3 sm:gap-6">
                 <!-- Presentation Title -->
-                <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-white">Presentation Title</dt>
+                <x-waitt.label for="form.name" value="{{ __('Presentation Title') }}"/>
                 <dd class="sm:col-span-2">
                     <input
-                        class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-xs mt-1 block"
+                        class="w-full border-gray-700 bg-gray-900 text-gray-300 focus:border-teal-600 focus:ring-teal-600 rounded-md shadow-xs mt-1 block"
                         type="text" maxlength="255" wire:model="form.name">
                     @error('form.name') <span class="error text-red-500">{{ $message }}</span> @enderror
                 </dd>
 
                 <!-- Presentation Description -->
-                <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-white">Presentation Description</dt>
+                <x-waitt.label for="form.description" value="{{ __('Presentation Description') }}"/>
                 <dd class="sm:col-span-2">
                 <textarea rows="7"
-                    class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-xs mt-1"
-                    wire:model="form.description" maxlength="300"></textarea>
+                          class="w-full border-gray-700 bg-gray-900 text-gray-300 focus:border-teal-600 focus:ring-teal-600 rounded-md shadow-xs mt-1 block"
+                          wire:model="form.description" maxlength="300"></textarea>
                     @error('form.description') <span class="error text-red-500">{{ $message }}</span> @enderror
                 </dd>
 
                 <!-- Presentation Type -->
-                <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-white">Presentation Type</dt>
+                <x-waitt.label for="form.presentation_type_id" value="{{ __('Presentation Type') }}"/>
                 <dd class="sm:col-span-2">
                     <select wire:model="form.presentation_type_id"
-                            class="mt-1 w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-900 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500">
+                            class="w-full border-gray-700 bg-gray-900 text-gray-300 focus:border-teal-600 focus:ring-teal-600 rounded-md shadow-xs mt-1 block">
                         @foreach($presentationTypes as $presentationType)
                             <option value={{$presentationType->id}}
                                 {{$presentationType->id == $presentation->presentation_type_id ? 'selected' : ''}}
@@ -45,11 +45,11 @@
                 </dd>
 
                 <!-- Difficulty Level -->
-                <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-white">Difficulty Level</dt>
+                <x-waitt.label for="name" value="{{ __('Difficulty Level') }}"/>
                 <dd class="sm:col-span-2">
                     <select wire:model="form.difficulty_id"
-                            class="mt-1 w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-900 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500">
-                        @foreach(App\Models\Difficulty::all() as $difficulty)
+                            class="w-full border-gray-700 bg-gray-900 text-gray-300 focus:border-teal-600 focus:ring-teal-600 rounded-md shadow-xs mt-1 block"
+                        @foreach(App\Models\Difficulty::all() as $difficulty)>
                             <option value="{{$difficulty->id}}">{{ucfirst($difficulty->level)}}</option>
                         @endforeach
                     </select>
@@ -57,10 +57,10 @@
                 </dd>
 
                 <!-- Suggested Max Participants -->
-                <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-white">Suggested Max Participants</dt>
+                <x-waitt.label for="name" value="{{ __('Suggested Max Participants') }}"/>
                 <dd class="sm:col-span-2">
                     <input
-                        class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-xs mt-1"
+                        class="w-full border-gray-700 bg-gray-900 text-gray-300 focus:border-teal-600 focus:ring-teal-600 rounded-md shadow-xs mt-1 block"
                         type="number" wire:model="form.max_participants">
                     @error('form.max_participants') <span class="error text-red-500">{{ $message }}</span> @enderror
                 </dd>
@@ -68,13 +68,12 @@
         </div>
     </x-slot>
 
-    <x-slot name="buttons" class="dark:bg-gray-900">
-        <x-secondary-button type="button" wire:click="$dispatch('closeModal')" class="mr-3">
+    <x-slot name="buttons">
+        <x-waitt.button type="button" wire:click="$dispatch('closeModal')" class="mr-3">
             {{ __('Cancel') }}
-        </x-secondary-button>
-        <button type="submit"
-                class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-hidden focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+        </x-waitt.button>
+        <x-waitt.button type="submit" variant="save">
             Save
-        </button>
+        </x-waitt.button>
     </x-slot>
-</x-livewire-modal>
+</x-waitt.livewire-modal>
