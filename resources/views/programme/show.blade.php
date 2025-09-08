d4584cb8f4d8c44d1cb214cb863268abd87a1124
Gijs Borghouts <borg0039@hz.nl>
2025-09-08 15:10:43 +0200

    minor style change to show page

--- resources/views/programme/show.blade.php
+++ resources/views/programme/show.blade.php
@@ -5,14 +5,14 @@
 
 <x-app-layout>
     <div class="container h-screen mx-auto px-6 py-12">
-        <h2 class="text-center text-waitt-yellow text-4xl font-extrabold bg-clip-text bg-linear-to-r from-pink-400 via-purple-400 to-blue-400 mb-12">
-            Programme / <span class=" font-bold text-waitt-pink">{{$presentation->name}}</span>
+        <h2 class="text-center {{$textColor}} text-4xl font-extrabold bg-clip-text bg-linear-to-r from-pink-400 via-purple-400 to-blue-400 mb-12">
+            Programme / <span class=" font-bold text-waitt-yellow">{{$presentation->name}}</span>
         </h2>
         <div
             class="bg-waitt-dark backdrop-blur-sm border border-slate-900 rounded-2xl p-8 shadow-xl overflow-hidden transform transition-all">
             <div class="absolute top-0 left-0 w-full h-2 {{$borderColor}}"></div>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6 pt-5 px-6 pb-5">
-                <div class=" {{$textColor}} ">
+                <div class="{{$textColor}}">
                     @if($presentation->speakers->count() > 1)
                         <h4 class="tracking-tight text-2xl font-bold pb-5 text-left">Speakers</h4>
                     @else
@@ -30,7 +30,7 @@
                         @endforeach
                     </div>
                 </div>
-                <div class="sm:col-span-2 text-waitt-pink">
+                <div class="sm:col-span-2 text-white">
                     <h2 class="text-3xl {{$textColor}} font-bold mb-5">{{$presentation->name}}</h2>
 
                     <div>
@@ -41,7 +41,7 @@
                     @if($presentation->company)
                         <div class="mt-5">
                             <h2 class="text-2xl font-bold">Company</h2>
-                            <a class="font-bold {{$linkColor}}"
+                            <a class="font-bold"
                                href="{{route('companies.show', $presentation->company)}}">{{$presentation->company->name}}</a>
                         </div>
                     @endif
@@ -56,7 +56,7 @@
                             Difficulty level
                         </h2>
                         <div
-                            class="text-yellow-500 flex transititext-primary text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600 p-2"
+                            class="text-white flex transititext-primary text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600 p-2"
                             data-te-toggle="tooltip"
                             title="{{ ucfirst($presentation->difficulty->description) }}">
                             @for($i = 0; $i < $presentation->difficulty->id; $i++)
@@ -65,7 +65,7 @@
                                           d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"/>
                                 </svg>
                             @endfor
-                                <p class="text-waitt-pink">
+                                <p class="text-white">
                                     - {{ucfirst($presentation->difficulty->level)}}</p>
 
                         </div>
