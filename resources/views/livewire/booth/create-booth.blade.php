f2ff3b31d8a2ed0e7e5918bd0e498920acaf5412
Valeria Stamenova <valeria.stamenova3@gmail.com>
2025-09-08 09:51:19 +0200

    Redesign the booth pages

--- resources/views/livewire/booth/create-booth.blade.php
+++ resources/views/livewire/booth/create-booth.blade.php
@@ -5,12 +5,12 @@
 
 <form wire:submit="save">
     @csrf
-    <x-action-section>
+    <x-waitt.action-section>
         <x-slot name="title">
             {{ __('Booth details') }}
         </x-slot>
         <x-slot name="description">
-            <div class="space-y-4 text-gray-800 dark:text-gray-200 text-sm">
+            <div class="space-y-4 text-gray-200 text-sm">
                 <p>Add manually the booth for an already registered company.</p>
                 <p class="mt-4">Total area sized mentioned in the sponsor packages:</p>
                 <ul class="list-disc ml-6">
@@ -22,9 +22,10 @@
         <x-slot name="content">
             <div class="pr-5">
                 <div class="col-span-6 sm:col-span-4 ">
-                    <x-label for="company_id" class="after:content-['*'] after:text-red-500"
-                             value="{{ __('Company name') }}"></x-label>
-                    <x-select wire:model="companyId" name="company_id" class="mt-1 block w-full">
+                    <x-waitt.label for="company_id" class="after:content-['*'] after:text-red-500"
+                                   value="{{ __('Company name') }}"></x-waitt.label>
+                    <select wire:model="companyId" name="company_id"
+                            class="w-full border-gray-700 bg-gray-900 text-gray-300 focus:border-teal-600 focus:ring-teal-600 rounded-md shadow-xs mt-1 block">
                         @foreach(Company::whereDoesntHave('booth')
                                     ->hasStatus(ApprovalStatus::APPROVED)->get() as $company)
                             <option value="{{ $company->id }}">
@@ -34,58 +35,63 @@
                                 @endif
                             </option>
                         @endforeach
-                    </x-select>
-                    @error('company')  <p class="mt-2 text-red-500"> {{$message}} </p> @enderror
+                    </select>
+                    @error('company') <p class="mt-2 text-red-500"> {{$message}} </p> @enderror
                 </div>
                 <div class="col-span-6 sm:col-span-4 pt-3">
-                    <x-label for="width" class="after:content-['*'] after:text-red-500"
-                             value="{{ __('Width') }}"></x-label>
-                    <x-input wire:model="width" name="width" type="number" step="0.1" min="1" class="mt-1 block w-full"></x-input>
-                    @error('width')  <p class="mt-2 text-red-500"> {{$message}} </p> @enderror
+                    <x-waitt.label for="width" class="after:content-['*'] after:text-red-500"
+                             value="{{ __('Width') }}"></x-waitt.label>
+                    <x-waitt.input wire:model="width" name="width" type="number" step="0.1" min="1"
+                             class="mt-1 block w-full"></x-waitt.input>
+                    @error('width') <p class="mt-2 text-red-500"> {{$message}} </p> @enderror
                 </div>
                 <div class="col-span-6 sm:col-span-4 pt-3">
-                    <x-label for="length" class="after:content-['*'] after:text-red-500"
-                             value="{{ __('Length') }}"></x-label>
-                    <x-input wire:model="length" name="length" step="0.1" min="1" type="number" class="mt-1 block w-full"></x-input>
-                    @error('length')  <p class="mt-2 text-red-500"> {{$message}} </p> @enderror
+                    <x-waitt.label for="length" class="after:content-['*'] after:text-red-500"
+                             value="{{ __('Length') }}"></x-waitt.label>
+                    <x-waitt.input wire:model="length" name="length" step="0.1" min="1" type="number"
+                             class="mt-1 block w-full"></x-waitt.input>
+                    @error('length') <p class="mt-2 text-red-500"> {{$message}} </p> @enderror
                 </div>
                 <div class="col-span-6 sm:col-span-4 pt-3">
-                    <x-label for="additionalInformation" value="{{ __('Additional information') }}"></x-label>
-                    <textarea wire:model="additionalInformation" maxlength="255"
+                    <x-waitt.label for="additionalInformation" value="{{ __('Additional information') }}"></x-waitt.label>
+                    <x-waitt.input-textarea wire:model="additionalInformation" maxlength="255"
                               class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-xs block mt-1 w-full"
                               name="description"
-                    ></textarea>
-                    @error('additionalInformation')  <p class="mt-2 text-red-500"> {{$message}} </p> @enderror
+                    ></x-waitt.input-textarea>
+                    @error('additionalInformation') <p class="mt-2 text-red-500"> {{$message}} </p> @enderror
                 </div>
                 <div class="mt-4">
                     <div>
-                        <x-label for="booth_owner" class="after:content-['*'] after:text-red-500"
-                                 value="{{ __('Choose company member to become the booth owner') }}"></x-label>
+                        <x-waitt.label for="booth_owner" class="after:content-['*'] after:text-red-500"
+                                 value="{{ __('Choose company member to become the booth owner') }}"></x-waitt.label>
                         <div>
                             <input
-                                class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-apricot-peach-500 dark:focus:border-apricot-peach-600 focus:ring-apricot-peach-500 dark:focus:ring-apricot-peach-600 rounded-md shadow-xs mt-1 block"
+                                class="w-full border-gray-700 bg-gray-900 text-gray-300 focus:border-teal-600 focus:ring-teal-600 rounded-md shadow-xs mt-1 block"
                                 type="text" maxlength="255" wire:focus="toggleDropdown"
                                 wire:model.live="searchValue">
-                            <div class="{{$isDropdownVisible ? 'block' : 'hidden'}} max-h-48 rounded-sm overflow-auto bg-white dark:bg-gray-900">
+                            <div
+                                class="{{$isDropdownVisible ? 'block' : 'hidden'}} max-h-48 rounded-sm overflow-auto bg-gray-900">
                                 <ul>
                                     @if($users)
                                         @forelse($users as $user)
                                             <li wire:click="selectUser({{$user->id}})" wire:key="{{$user->id}}"
                                                 class="hover:cursor-pointer w-full" onclick="event.stopPropagation()">
                                                 <div
-                                                    class="bg-white hover:bg-gray-100 dark:bg-gray-800 shadow-sm rounded-md p-2 flex items-center space-x-3">
-                                                    <img class="h-8 w-8 rounded-full shrink-0" src="{{ $user->profile_photo_url }}"
+                                                    class="bg-waitt-dark hover:bg-slate-800 transition-all shadow-sm rounded-md p-2 flex items-center space-x-3">
+                                                    <img class="h-8 w-8 rounded-full shrink-0"
+                                                         src="{{ $user->profile_photo_url }}"
                                                          alt="{{ $user->name }}">
                                                     <div class="flex-1 min-w-0">
-                                                        <p class="text-sm font-medium text-gray-900 dark:text-gray-100 truncate">{{ $user->name }}</p>
-                                                        <p class="text-xs text-gray-500 dark:text-gray-400 truncate">{{ $user->email }}</p>
+                                                        <p class="text-sm font-medium text-gray-100 truncate">{{ $user->name }}</p>
+                                                        <p class="text-xs text-gray-400 truncate">{{ $user->email }}</p>
                                                     </div>
                                                 </div>
                                             </li>
                                         @empty
                                             <li class="py-2">
-                                                <p class="text-sm font-medium text-gray-700 dark:text-gray-100 truncate">No results
-                                                                                                                         found.</p>
+                                                <p class="text-sm font-medium text-gray-700 dark:text-gray-100 truncate">
+                                                    No results
+                                                    found.</p>
                                             </li>
                                         @endforelse
                                     @endif
@@ -96,7 +102,7 @@
                             <span
                                 class="text-xs text-crew-400 dark:text-crew-200">The user already has the following roles: {{ $selectedUser->allRoles->implode(',') }}</span>
                         @endif
-                        @error('selectedUser')  <p class="mt-2 text-red-500"> {{$message}} </p> @enderror
+                        @error('selectedUser') <p class="mt-2 text-red-500"> {{$message}} </p> @enderror
                     </div>
                 </div>
             </div>
@@ -108,5 +114,5 @@
                 Save
             </x-button>
         </x-slot>
-    </x-action-section>
+    </x-waitt.action-section>
 </form>
--- resources/views/livewire/booth/edit-booth-modal.blade.php
+++ resources/views/livewire/booth/edit-booth-modal.blade.php
@@ -1,4 +1,4 @@
-<x-livewire-modal form-action="save">
+<x-waitt.livewire-modal form-action="save">
     <x-slot name="title" class="dark:bg-gray-900 border-gray-800">
         Edit booth
     </x-slot>
@@ -10,25 +10,25 @@
     <x-slot name="content" class="w-full dark:bg-gray-800">
         <div class="px-4 py-6 sm:px-0">
             <dl class="sm:grid sm:grid-cols-3 sm:gap-6">
-                <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-white">Booth width</dt>
+                <x-waitt.label for="form.width" value="{{ __('Booth width') }}"/>
                 <dd class="sm:col-span-2">
                     <input
-                        class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-xs mt-1 block"
+                        class="w-full border-gray-700 bg-gray-900 text-gray-300 focus:border-teal-600 focus:ring-teal-600 rounded-md shadow-xs mt-1 block"
                         type="number" min="1" step=".1" wire:model="form.width">
                     @error('form.width') <span class="error text-red-500">{{ $message }}</span> @enderror
                 </dd>
-                <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-white">Booth length</dt>
+                <x-waitt.label for="form.length" value="{{ __('Booth length') }}"/>
                 <dd class="sm:col-span-2">
                     <input
-                        class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-xs mt-1 block"
+                        class="w-full border-gray-700 bg-gray-900 text-gray-300 focus:border-teal-600 focus:ring-teal-600 rounded-md shadow-xs mt-1 block"
                         type="number" min="1" step=".1" wire:model="form.length">
                     @error('form.length') <span class="error text-red-500">{{ $message }}</span> @enderror
                 </dd>
                 <!-- Presentation Description -->
-                <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-white">Booth additional info</dt>
+                <x-waitt.label for="form.additional_information" value="{{ __('Booth additional info') }}"/>
                 <dd class="sm:col-span-2">
                 <textarea
-                    class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-xs mt-1"
+                    class="w-full border-gray-700 bg-gray-900 text-gray-300 focus:border-teal-600 focus:ring-teal-600 rounded-md shadow-xs mt-1 block"
                     wire:model="form.additional_information" maxlength="255"></textarea>
                     @error('form.additional_information') <span class="error text-red-500">{{ $message }}</span> @enderror
                 </dd>
@@ -37,12 +37,11 @@
     </x-slot>
 
     <x-slot name="buttons" class="dark:bg-gray-900">
-        <x-secondary-button type="button" wire:click="$dispatch('closeModal')" class="mr-3">
+        <x-waitt.button type="button" wire:click="$dispatch('closeModal')" class="mr-3">
             {{ __('Cancel') }}
-        </x-secondary-button>
-        <button type="submit"
-                class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-hidden focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+        </x-waitt.button>
+        <x-waitt.button type="submit" variant="save">
             Save
-        </button>
+        </x-waitt.button>
     </x-slot>
-</x-livewire-modal>
+</x-waitt.livewire-modal>
