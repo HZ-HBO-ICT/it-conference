927033ee94fe60e576327adeb61c3aa4a9c4e093
Ihor Novikov <71547534+IGORnvk@users.noreply.github.com>
2025-09-11 00:21:49 +0200

    Update index.blade.php

--- resources/views/crew/tickets/index.blade.php
+++ resources/views/crew/tickets/index.blade.php
@@ -1,23 +1,26 @@
 <x-crew-colorful-layout>
     <div class="flex items-center justify-between mt-5">
-        <h2 class="font-semibold text-3xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-3xl text-gray-200 leading-tight">
             {{ __('Scan tickets') }}
         </h2>
-        <div class="pt-5">
-            <x-waitt.list-section>
-                <x-slot name="content">
-                    @if($edition->is_in_progress)
-                        <x-button onclick="Livewire.dispatch('openModal', { component: 'qr-code.scanner' })"
-                                  class="sm:hidden">{{ __('Start Scanning') }}</x-button>
+    </div>
 
-                        <div class="hidden sm:block" id="qr-reader"></div>
+    <div class="pt-5">
+        <x-waitt.list-section>
+            <x-slot name="content">
+                @if($edition->is_in_progress)
+                    <x-button onclick="Livewire.dispatch('openModal', { component: 'qr-code.scanner' })"
+                              class="sm:hidden">{{ __('Start Scanning') }}</x-button>
 
-                        <p id="errorMessage"></p>
-                    @else
+                    <div class="hidden sm:block" id="qr-reader"></div>
+
+                    <p id="errorMessage"></p>
+                @else
+                    <div class="h-screen text-white text-xl">
                         Ticket scanning will be available on the day of the conference.
-                    @endif
-                </x-slot>
-            </x-waitt.list-section>
-        </div>
+                    </div>
+                @endif
+            </x-slot>
+        </x-waitt.list-section>
     </div>
 </x-crew-colorful-layout>
