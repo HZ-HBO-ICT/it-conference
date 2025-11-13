7ff1bf36f07b97bd5b7d734e5ccbaa1a4606af09
Tim Kardol <54664131+TimKardol@users.noreply.github.com>
2025-11-13 09:09:16 +0100

    Bugfixes mobile and crew (#782)
    Co-authored-by: Valeria Stamenova <valeria.stamenova3@gmail.com>

--- resources/views/components/programme/default-presentation.blade.php
+++ resources/views/components/programme/default-presentation.blade.php
@@ -1,6 +1,5 @@
 @php use Carbon\Carbon; @endphp
-<div class=" w-full pt-0.5 px-2"
-     style="height: {{ Carbon::parse($presentation->start)->diffInMinutes(Carbon::parse($presentation->end)) * (14 / 30) * 0.25}}rem;">
+<div class="w-full py-3">
     <div
         class="w-full h-full rounded-sm bg-waitt-yellow max-md:h-3/4 max-md:bg-waitt-dark/70 max-md:rounded-xl max-md:p-4 max-md:shadow max-md:border max-md:border-waitt-yellow max-md:text-waitt-yellow"
     >
